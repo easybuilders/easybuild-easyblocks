4034505249f2f8823e6f9d9ee65f83ae7aa9d0f5
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-12-17 18:38:46 +0100

    initial cleanup in header/imports

--- easybuild/easyblocks/o/opensees.py
+++ easybuild/easyblocks/o/opensees.py
@@ -1,5 +1,5 @@
 ##
-# Copyright 2009-2013 Ghent University
+# Copyright 2013 Ghent University
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
@@ -23,24 +23,19 @@
 # along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 """
-EasyBuild support for building and installing OpenSeed, implemented as an easyblock
+EasyBuild support for building and installing OpenSees, implemented as an easyblock
+
 @author: Bart Verleye (the University of Auckland)
 """
 import os
-import stat
-from distutils.version import LooseVersion
 
-import easybuild.tools.environment as env
-import easybuild.tools.toolchain as toolchain
-from easybuild.framework.easyblock import EasyBlock
-from easybuild.tools.filetools import run_cmd, adjust_permissions
+from easybuild.tools.filetools import run_cmd
 from easybuild.tools.modules import get_software_root
 from easybuild.easyblocks.generic.makecp import MakeCp
 
 
 class EB_OpenSees(MakeCp):
     """Support for building and installing OpenSees."""
-
 
     def configure_step(self):
          f=open('Makefile.def','w');
