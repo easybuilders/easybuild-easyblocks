ed6f1cf63e7e1ff79dab4e9d9e587a4b30ceb9d4
Georg Rath <georg.rath@meduniwien.ac.at>
2014-02-14 10:20:29 +0100

    Changed license to the correct one.

--- easybuild/easyblocks/s/spm.py
+++ easybuild/easyblocks/s/spm.py
@@ -1,32 +1,13 @@
-##
-# Copyright 2009-2013 Ghent University
-#
-# This file is part of EasyBuild,
-# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
-# with support of Ghent University (http://ugent.be/hpc),
-# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
-# the Hercules foundation (http://www.herculesstichting.be/in_English)
-# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
-#
-# http://github.com/hpcugent/easybuild
-#
-# EasyBuild is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation v2.
-#
-# EasyBuild is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
-##
+# EasyBlock for SPM:
+# ----------------------------------------------------------------------------
+#   Copyright: 2014 - Medical University of Vienna
+#   License: MIT
+#   Authors: Georg <georg.rath@meduniwien.ac.at
+# ----------------------------------------------------------------------------
 """
 EasyBuild support for SPM, implemented as an easyblock
 
-@authors: Stijn De Weirdt (UGent), Dries Verdegem (UGent), Kenneth Hoste (UGent), Pieter De Baets (UGent),
-          Jens Timmerman (UGent), Georg Rath (Medical University of Vienna)
+@authors: Georg Rath (Medical University of Vienna)
 """
 import os
 import shutil
