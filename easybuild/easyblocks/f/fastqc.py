90e4f53f4d2ae4b0e3a902c8566a631829156df3
Andreas Panteli <a.panteli@cyi.ac.cy>
2014-04-11 12:31:58 +0300

    Contribute block for FastQC

---
+++ easybuild/easyblocks/f/fastqc.py
@@ -0,0 +1,67 @@
+##
+# Copyright 2009-2013 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBlock for FastQC adapted from Packed Binary
+
+@author: Jens Timmerman (Ghent University), Andreas Panteli (The Cyprus Institute)
+"""
+import os
+import shutil
+
+from easybuild.framework.easyblock import EasyBlock
+from easybuild.easyblocks.generic.binary import Binary
+
+
+class EB_FastQC(Binary, EasyBlock):
+    """Support for installing packed binary software.
+    Just unpack the sources in the install dir and change mod 0755 fastqc
+    """
+
+    def extract_step(self):
+        """Unpack the source"""
+        EasyBlock.extract_step(self)
+
+    def install_step(self):
+        """Copy all unpacked source directories to install directory, one-by-one."""
+        try:
+            os.chdir(self.builddir)
+            for src in os.listdir(self.builddir):
+                srcpath = os.path.join(self.builddir, src)
+                if os.path.isdir(srcpath):
+                    # copy files to install dir via Binary
+                    self.cfg['start_dir'] = src
+                    Binary.install_step(self)
+                elif os.path.isfile(srcpath):
+                    shutil.copy2(srcpath, self.installdir)
+                else:
+                    self.log.error("Path %s is not a file nor a directory?" % srcpath)
+        except OSError, err:
+            self.log.error("Failed to copy unpacked sources to install directory: %s" % err)
+	#Change mod 755 fastqc
+	try:
+            dst = os.path.join(self.installdir, 'fastqc')
+	    os.chmod(dst, 0755)
+	except OSError, err:
+	    self.log.error("Failed to chmod fastqc in the install directory: %s" % err)
