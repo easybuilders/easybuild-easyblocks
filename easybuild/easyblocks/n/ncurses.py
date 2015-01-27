f67abb04c8ec036b364df261abd80b13c02ea2ea
Ward Poelmans <wpoely86@gmail.com>
2015-01-27 12:37:53 +0100

    Just give deprecated warning

--- easybuild/easyblocks/n/ncurses.py
+++ easybuild/easyblocks/n/ncurses.py
@@ -15,7 +15,6 @@ Easybuild support for building ncurses, implemented as an easyblock
 @author: Cedric Laczny (Uni.Lu)
 @author: Fotis Georgatos (Uni.Lu)
 @author: Kenneth Hoste (Ghent University)
-@author: Ward Poelmans (Ghent University)
 """
 
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
@@ -28,4 +27,32 @@ class EB_ncurses(ConfigureMake):
     def __init__(self, *args, **kwargs):
         super(EB_ncurses, self).__init__(*args, **kwargs)
         url = "https://github.com/hpcugent/easybuild-easyconfigs/tree/develop/easybuild/easyconfigs/n/ncurses"
-        self.log.error("EB_ncurses: easyblock has been removed in favor of new easyconfigs: %s" % url)
+        self.log.deprecated("EB_ncurses: easyblock has been removed in favor of new easyconfigs: %s" % url, "3.0")
+
+    def configure_step(self):
+        """
+        No configure
+        """
+        self.cfg.update('configopts', '--with-shared --enable-overwrite')
+        super(EB_ncurses, self).configure_step()
+
+    def sanity_check_step(self):
+        """Custom sanity check for ncurses."""
+
+        custom_paths = {
+            'files': ['bin/%s' % x for x in ["captoinfo", "clear", "infocmp", "infotocap", "ncurses5-config",
+                                              "reset", "tabs", "tic", "toe", "tput", "tset"]] +
+            ['lib/lib%s.a' % x for x in ["form", "form", "menu", "menu_g", "ncurses", "ncurses++",
+                                         "ncurses_g", "panel", "panel_g"]],
+            'dirs': ['include']
+        }
+
+        super(EB_ncurses, self).sanity_check_step(custom_paths=custom_paths)
+
+    def make_module_req_guess(self):
+        """
+        Set correct CPLUS path.
+        """
+        guesses = super(EB_ncurses, self).make_module_req_guess()
+        guesses.update({'CPLUS': ['include/ncurses']})  # will only be present without --enable-overwrite
+        return guesses
