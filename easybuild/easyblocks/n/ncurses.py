77f1016ceca4be2b4fbcbc84b97b16fc4388c386
Ward Poelmans <wpoely86@gmail.com>
2015-01-27 12:17:53 +0100

    Give error if the ncurses easyblock is used

---
+++ easybuild/easyblocks/n/ncurses.py
@@ -0,0 +1,30 @@
+##
+# This file is an EasyBuild reciPY as per https://github.com/hpcugent/easybuild
+#
+# Copyright:: Copyright 2012-2014 Uni.Lu/LCSB, NTUA
+# Authors::   Cedric Laczny <cedric.laczny@uni.lu>, Fotis Georgatos <fotis@cern.ch>, Kenneth Hoste
+# License::   MIT/GPL
+# $Id$
+#
+# This work implements a part of the HPCBIOS project and is a component of the policy:
+# http://hpcbios.readthedocs.org/en/latest/HPCBIOS_2012-90.html
+##
+"""
+Easybuild support for building ncurses, implemented as an easyblock
+
+@author: Cedric Laczny (Uni.Lu)
+@author: Fotis Georgatos (Uni.Lu)
+@author: Kenneth Hoste (Ghent University)
+@author: Ward Poelmans (Ghent University)
+"""
+
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
+
+
+class EB_ncurses(ConfigureMake):
+    """
+    Old easyblock for ncurses, superseded by easyconfigs
+    """
+    def __init__(self, *args, **kwargs):
+        url = "https://github.com/hpcugent/easybuild-easyconfigs/tree/develop/easybuild/easyconfigs/n/ncurses"
+        self.log.error("EB_ncurses: easyblock has been removed in favor of new easyconfigs: %s" % url)
