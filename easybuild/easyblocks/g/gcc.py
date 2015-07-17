6c73a00ec67eb410cc94bae4e8ec3361657e6821
Jens Timmerman <jens.timmerman@ugent.be>
2015-07-17 15:52:57 +0200

    Update gcc.py
    a recent llvm breaks the comparisonprocess between stage 2 and 3 for some reason. This should fix it, see https://github.com/Homebrew/homebrew/issues/38501

--- easybuild/easyblocks/g/gcc.py
+++ easybuild/easyblocks/g/gcc.py
@@ -410,6 +410,10 @@ class EB_GCC(ConfigureMake):
 
             # enable bootstrapping for self-containment
             configopts += " --enable-bootstrap "
+            # Use 'bootstrap-debug' build configuration to force stripping of object
+            # files prior to comparison during bootstrap (broken by Xcode 6.3).
+            # see https://github.com/Homebrew/homebrew/issues/38501
+            configopts += "--with-build-config=bootstrap-debug",
 
             # PPL config options
             if self.cfg['withppl']:
