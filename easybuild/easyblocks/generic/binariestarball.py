0ce1c19edb972d6a32752d4b00db8ed2804426b5
Benjamin Roberts <b.roberts@auckland.ac.nz>
2015-10-27 09:37:49 +1300

    Update BinariesTarball to include support for an explicit list of binary files if one is provided

--- easybuild/easyblocks/generic/binariestarball.py
+++ easybuild/easyblocks/generic/binariestarball.py
@@ -32,6 +32,7 @@ import shutil
 import stat
 
 from easybuild.easyblocks.generic.tarball import Tarball
+from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.filetools import adjust_permissions
 
@@ -40,21 +41,43 @@ class BinariesTarball(Tarball):
     """
     Support for installing a tarball of binaries
     """
+    @staticmethod
+    def extra_options(extra_vars=None):
+        """
+        Define list of files to be copied during the installation process
+        """
+        extra_vars = Tarball.extra_options(extra_vars)
+        # Allow specification of files to copy explicitly.
+        # If files_to_copy is a zero-length list, all files (but not
+        # directories, etc.) in start_dir will be copied with their
+        # current names.
+        extra_vars['files_to_copy'] = [
+            [],
+            "List of optional (source_file, destination_file) tuples",
+            CUSTOM,
+        ]
+        return extra_vars
 
     def install_step(self):
         """Install by copying unzipped binaries to 'bin' subdir of installation dir, and fixing permissions."""
 
         bindir = os.path.join(self.installdir, 'bin')
+        items_to_copy = []
         try:
             os.makedirs(bindir)
-            for item in os.listdir(self.cfg['start_dir']):
+            if len(self.cfg['files_to_copy']) == 0:
+                for item in os.listdir(self.cfg['start_dir']):
+                    items_to_copy.append((os.path.join(self.cfg['start_dir'], item), item))
+            else:
+                items_to_copy = self.cfg['files_to_copy']
+            for (item, destname) in items_to_copy:
                 if os.path.isfile(item):
-                    shutil.copy2(os.path.join(self.cfg['start_dir'], item), bindir)
+                    shutil.copy2(item, os.path.join(bindir, destname))
                     # make sure binary has executable permissions
-                    adjust_permissions(os.path.join(bindir, item), stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH, add=True)
+                    adjust_permissions(os.path.join(bindir, destname), stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH, add=True)
                     self.log.debug("Copied %s to %s and fixed permissions" % (item, bindir))
                 else:
-                    self.log.warning("Skipping non-file %s in %s, not copying it." % (item, self.cfg['start_dir']))
+                    self.log.warning("%s: not a file. Skipping." % item)
         except OSError, err:
-            raise EasyBuildError("Copying binaries in %s to install dir 'bin' failed: %s", self.cfg['start_dir'], err)
+            raise EasyBuildError("Copying binaries to install dir 'bin' failed: %s" % err)
 
