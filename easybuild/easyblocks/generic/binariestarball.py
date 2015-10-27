6f9be2ed5cb2ccfb209b9c9004d0539b27fd47ca
Benjamin Roberts <b.roberts@auckland.ac.nz>
2015-10-28 11:19:36 +1300

    Some changes recommended by K.H.

--- easybuild/easyblocks/generic/binariestarball.py
+++ easybuild/easyblocks/generic/binariestarball.py
@@ -51,11 +51,9 @@ class BinariesTarball(Tarball):
         # If files_to_copy is a zero-length list, all files (but not
         # directories, etc.) in start_dir will be copied with their
         # current names.
-        extra_vars['files_to_copy'] = [
-            [],
-            "List of optional (source_file, destination_file) tuples",
-            CUSTOM,
-        ]
+        extra_vars.update({
+            'files_to_copy': [[], "List of optional (source_file, destination_file) tuples", CUSTOM,]
+        })
         return extra_vars
 
     def install_step(self):
@@ -69,15 +67,14 @@ class BinariesTarball(Tarball):
                 for item in os.listdir(self.cfg['start_dir']):
                     items_to_copy.append((os.path.join(self.cfg['start_dir'], item), item))
             else:
-                items_to_copy = self.cfg['files_to_copy']
+                items_to_copy.append(os.path.join(self.cfg['start_dir'], item) for item in self.cfg['files_to_copy'])
             for (item, destname) in items_to_copy:
                 if os.path.isfile(item):
                     shutil.copy2(item, os.path.join(bindir, destname))
                     # make sure binary has executable permissions
                     adjust_permissions(os.path.join(bindir, destname), stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH, add=True)
-                    self.log.debug("Copied %s to %s and fixed permissions" % (item, bindir))
+                    self.log.debug("Copied %s to %s and fixed permissions", item, bindir)
                 else:
-                    self.log.warning("%s: not a file. Skipping." % item)
+                    self.log.warning("%s: not a file. Skipping.", item)
         except OSError, err:
-            raise EasyBuildError("Copying binaries to install dir 'bin' failed: %s" % err)
-
+            raise EasyBuildError("Copying binaries to install dir 'bin' failed: %s", err)
