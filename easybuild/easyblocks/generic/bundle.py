c15ca3ff3b5f0e40ebb9cc69ccd9b9cf64659872
Kenneth Hoste <kenneth.hoste@ugent.be>
2017-10-20 12:30:55 +0200

    allow skipping of steps for Bundle components + fix issues with templates & formatting of error message

--- easybuild/easyblocks/generic/bundle.py
+++ easybuild/easyblocks/generic/bundle.py
@@ -123,7 +123,7 @@ class Bundle(EasyBlock):
         for idx, cfg in enumerate(self.comp_cfgs):
             easyblock = cfg.get('easyblock') or self.cfg['default_easyblock']
             if easyblock is None:
-                raise EasyBuildError("No easyblock specified for component %d v%d", cfg['name'], cfg['version'])
+                raise EasyBuildError("No easyblock specified for component %s v%s", cfg['name'], cfg['version'])
             elif easyblock == 'Bundle':
                 raise EasyBuildError("The '%s' easyblock can not be used to install components in a bundle", easyblock)
 
@@ -140,10 +140,11 @@ class Bundle(EasyBlock):
             comp.guess_start_dir()
 
             # run relevant steps
-            comp.patch_step()
-            comp.configure_step()
-            comp.build_step()
-            comp.install_step()
+            for step_name in ['patch', 'configure', 'build', 'install']:
+                if step_name in cfg['skipsteps']:
+                    comp.log.info("Skipping '%s' step for component %s v%s", step_name, cfg['name'], cfg['version'])
+                else:
+                    comp.run_step(step_name, [lambda x: getattr(x, '%s_step' % step_name)])
 
             # update environment to ensure stuff provided by former components can be picked up by latter components
             # once the installation is finalised, this is handled by the generated module
