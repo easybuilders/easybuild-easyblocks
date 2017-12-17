df31c24c9fa51e30e3600ff24d24cc9099965f32
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2017-12-17 12:48:09 +0100

    Adjust comsol.py accotrding to suggestions by Kenneth.

--- easybuild/easyblocks/c/comsol.py
+++ easybuild/easyblocks/c/comsol.py
@@ -25,6 +25,7 @@
 """
 EasyBuild support for installing Comsol, implemented as an easyblock
 
+@author: Mikael OEhman (Chalmers University of Technology)
 @author: Ake Sandgren (HPC2N, Umea University)
 """
 import re
@@ -36,7 +37,7 @@ import easybuild.tools.environment as env
 from easybuild.easyblocks.generic.packedbinary import PackedBinary
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
-from easybuild.tools.filetools import adjust_permissions, find_flexlm_license, read_file, write_file
+from easybuild.tools.filetools import adjust_permissions, copy_file, find_flexlm_license, read_file, write_file
 from easybuild.tools.modules import get_software_root
 from easybuild.tools.run import run_cmd
 from easybuild.tools.systemtools import get_shared_lib_ext
@@ -56,11 +57,11 @@ class EB_Comsol(PackedBinary):
 
         # The tar file comes from the DVD and has 0444 as permission at
         # the top dir.
-        adjust_permissions(self.cfg['start_dir'], stat.S_IWUSR)
+        adjust_permissions(self.start_dir, stat.S_IWUSR)
 
         default_lic_env_var = 'LM_LICENSE_FILE'
         lic_specs, self.license_env_var = find_flexlm_license(custom_env_vars=[default_lic_env_var],
-            lic_specs=[self.cfg['license_file']])
+                                                              lic_specs=[self.cfg['license_file']])
 
         if lic_specs:
             if self.license_env_var is None:
@@ -76,52 +77,48 @@ class EB_Comsol(PackedBinary):
             msg += "specify 'license_file', or define $LM_LICENSE_FILE"
             raise EasyBuildError(msg)
 
-        try:
-            shutil.copyfile(os.path.join(self.cfg['start_dir'], 'setupconfig.ini'), self.configfile)
-            config = read_file(self.configfile)
+        copy_file(os.path.join(self.start_dir, 'setupconfig.ini'), self.configfile)
+        config = read_file(self.configfile)
 
-            regdest = re.compile(r"^installdir\s*=.*", re.M)
-            reggui = re.compile(r"^showgui\s*=.*", re.M)
-            regagree = re.compile(r"^agree\s*=.*", re.M)
-            reglicpath = re.compile(r"^license\s*=.*", re.M)
-            reglinuxlauncher = re.compile(r"^linuxlauncher\s*=.*", re.M)
-            regsymlinks = re.compile(r"^symlinks\s*=.*", re.M)
-            regfileassoc = re.compile(r"^fileassoc\s*=.*", re.M)
+        config_vars = {
+            'agree': '1',
+            'desktopshortcuts': '0',
+            'fileassoc': '0',
+            'firewall': '0',
+            'installdirs': self.installdir,
+            'license': self.license_file,
+            'licmanager': '0',
+            'linuxlauncher': '0',
+            'showgui': '0',
+            'startmenushortcuts': '0',
+            'symlinks': '0',
+        }
 
-            config = regdest.sub("installdir=%s" % self.installdir, config)
-            config = reggui.sub("showgui=0", config)
-            config = regagree.sub("agree=1", config)
-            config = reglicpath.sub("license=%s" % self.license_file, config)
-            config = reglinuxlauncher.sub("linuxlauncher=0", config)
-            config = regsymlinks.sub("symlinks=0", config)
-            config = regfileassoc.sub("fileassoc=0", config)
+        matlab_root = get_software_root("MATLAB")
+        if matlab_root:
+            config_vars.update('matlabdir': matlab_root)
 
-            matlab_root = get_software_root("MATLAB")
-            if matlab_root:
-                regmatlab = re.compile(r"^matlabdir\s*=.*", re.M)
-                config = regmatlab.sub("matlabdir=%s" % matlabroot, config)
+        for key, val in config_vars.items():
+            regex = re.compile(r"^%s\s*=.*" % key, re.M)
+            config = regex.sub("%s=%s" % (key, val), config)
 
-            write_file(self.configfile, config)
-
-        except IOError, err:
-            raise EasyBuildError("Failed to create installation config file %s: %s", self.configfile, err)
+        write_file(self.configfile, config)
 
         self.log.debug('configuration file written to %s:\n %s', self.configfile, config)
 
     def install_step(self):
         """COMSOL install procedure using 'install' command."""
 
-        src = os.path.join(self.cfg['start_dir'], 'setup')
+        src = os.path.join(self.start_dir, 'setup')
 
         # make sure setup script is executable
         adjust_permissions(src, stat.S_IXUSR)
 
         # make sure $DISPLAY is not defined, which may lead to (hard to trace) problems
         # this is a workaround for not being able to specify --nodisplay to the install scripts
-        if 'DISPLAY' in os.environ:
-            os.environ.pop('DISPLAY')
+        env.unset_env_vars(['DISPLAY'])
 
-        cmd = "%s ./setup -s %s %s" % (self.cfg['preinstallopts'], self.configfile, self.cfg['installopts'])
+        cmd = ' '.join(self.cfg['preinstallopts'], src, '-s', self.configfile, self.cfg['installopts'])
         run_cmd(cmd, log_all=True, simple=True)
 
     def sanity_check_step(self):
