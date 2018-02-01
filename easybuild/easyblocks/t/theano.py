22307e2076c753d7f90bd909b7d6be71b0126d84
Samuel Moors <samuel.moors@vub.be>
2018-02-01 16:43:51 +0100

    move random string to end; make sure theano uses BLAS libraries

--- easybuild/easyblocks/t/theano.py
+++ easybuild/easyblocks/t/theano.py
@@ -1,20 +1,25 @@
 from easybuild.easyblocks.generic.pythonpackage import PythonPackage
+import os
 import random
 import string
 
 
 class EB_Theano(PythonPackage):
-    """ include a random string in compiledir_format to fix problems with architecture-specific compilation
-    when running the software on a heterogeneous compute cluster. """
-
+    """
+    1) Include a random string in compiledir_format to fix problems with architecture-specific compilation
+       when running the software on a heterogeneous compute cluster.
+    2) Make sure Theano uses the BLAS libraries.
+    """
     def make_module_extra(self):
 
         txt = super(EB_Theano, self).make_module_extra()
 
         rand_string = ''.join(random.choice(string.letters) for i in range(10))
 
-        theano_flags = ('compiledir_format=compiledir_%s-%%(short_platform)s-%%(processor)s-'
-                        '%%(python_version)s-%%(python_bitwidth)s' % rand_string)
+        theano_flags = ('compiledir_format=compiledir_%%(short_platform)s-%%(processor)s-'
+                        '%%(python_version)s-%%(python_bitwidth)s-%s' % rand_string)
+
+        theano_flags += ',blas.ldflags="%s"' % os.getenv('LIBBLAS')
 
         txt += self.module_generator.set_environment('THEANO_FLAGS', theano_flags)
 
