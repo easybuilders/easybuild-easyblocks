1092f02b11a5cff4f500e6216567b4cc38f2dd31
Samuel Moors <samuel.moors@vub.be>
2018-02-02 14:49:16 +0100

    make sure LIBBLAS is defined

--- easybuild/easyblocks/t/theano.py
+++ easybuild/easyblocks/t/theano.py
@@ -1,3 +1,34 @@
+##
+# Copyright 2018 Vrije Universiteit Brussel (VUB)
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for Theano, implemented as an easyblock
+
+@author: Samuel Moors (Vrije Universiteit Brussel)
+"""
+
+
 from easybuild.easyblocks.generic.pythonpackage import PythonPackage
 import os
 import random
@@ -10,6 +41,13 @@ class EB_Theano(PythonPackage):
        when running the software on a heterogeneous compute cluster.
     2) Make sure Theano uses the BLAS libraries.
     """
+    def configure_step(self):
+
+        self.LIBBLAS = os.getenv('LIBBLAS')
+
+        super(EB_Theano, self).configure_step()
+
+
     def make_module_extra(self):
 
         txt = super(EB_Theano, self).make_module_extra()
@@ -19,7 +57,7 @@ class EB_Theano(PythonPackage):
         theano_flags = ('compiledir_format=compiledir_%%(short_platform)s-%%(processor)s-'
                         '%%(python_version)s-%%(python_bitwidth)s-%s' % rand_string)
 
-        theano_flags += ',blas.ldflags="%s"' % os.getenv('LIBBLAS')
+        theano_flags += ',blas.ldflags="%s"' % self.LIBBLAS
 
         txt += self.module_generator.set_environment('THEANO_FLAGS', theano_flags)
 
