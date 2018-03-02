a2151b472e7d4d1860b2a2563b99fcc3b32803a0
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2018-03-02 21:34:21 +0100

    Make gromacs easyblock select build type from toolchainopt debug setting.

--- easybuild/easyblocks/g/gromacs.py
+++ easybuild/easyblocks/g/gromacs.py
@@ -179,8 +179,11 @@ class EB_GROMACS(CMakeMake):
 
                 run_cmd(plumed_cmd, log_all=True, simple=True)
 
-            # build a release build
-            self.cfg.update('configopts', "-DCMAKE_BUILD_TYPE=Release")
+            # Select debug or release build
+            if self.toolchain.options.get('debug', None):
+                self.cfg.update('configopts', "-DCMAKE_BUILD_TYPE=Debug")
+            else:
+                self.cfg.update('configopts', "-DCMAKE_BUILD_TYPE=Release")
 
             # prefer static libraries, if available
             if self.toolchain.options.get('dynamic', False):
