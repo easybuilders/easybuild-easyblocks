a3ef9079414140424b1b3f4d9c239950f83bf63f
ruben.dibattista@ifpen.fr <rubendibattista@gmail.com>
2018-04-09 18:18:43 +0200

    Use paths from self.

--- easybuild/easyblocks/l/lua.py
+++ easybuild/easyblocks/l/lua.py
@@ -33,14 +33,12 @@ i.e. configure/make/make install, implemented as an easyblock.
 @author: Jens Timmerman (Ghent University)
 @author: Toon Willems (Ghent University)
 """
-import fileinput
 import os
-import re
-import sys
 
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
-from easybuild.tools.run import run_cmd
 from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.filetools import apply_regex_substitutions
+from easybuild.tools.run import run_cmd
 
 
 class EB_Lua(ConfigureMake):
@@ -55,18 +53,13 @@ class EB_Lua(ConfigureMake):
         Lua does not need a configure step. In this step we just patch the
         `luaconf.h` file in the sources to point to the correct Lua Root
         """
-
         src_dir = self.src[0]['finalpath']
-
         luaconf_h = os.path.join(src_dir, 'src', 'luaconf.h')
 
         # Open file
         self.log.debug("Patching luaconf.h at {}".format(luaconf_h))
-        for line in fileinput.input(luaconf_h, inplace=True):
-            if line.startswith('#define LUA_ROOT'):
-                installdir = os.path.join(self.installdir, '')  # Add trailing slash
-                line = re.sub(r'/usr/local/', installdir, line)
-            sys.stdout.write(line)
+        installdir = os.path.join(self.installdir, '')  # Add trailing slash
+        apply_regex_substitutions(luaconf_h, [(r'/usr/local/', installdir)])
 
     def test_step(self):
         out = super(EB_Lua, self).test_step()
@@ -77,7 +70,7 @@ class EB_Lua(ConfigureMake):
 
         stdout, exitcode = run_cmd(cmd, log_all=True, simple=False)
 
-        if not(self.installdir in stdout):
+        if self.installdir not in stdout:
             raise EasyBuildError("package.path seems to not point in the right path")
 
         return out
@@ -88,9 +81,5 @@ class EB_Lua(ConfigureMake):
         - typical: make install
         """
 
-        cmd = "%s make install INSTALL_TOP=%s %s" % (self.cfg['preinstallopts'],
-                                                     self.install_subdir, self.cfg['installopts'])
-
-        (out, _) = run_cmd(cmd, log_all=True, simple=False)
-
-        return out
+        self.cfg.update('installopts', 'INSTALL_TOP=%s' % self.installdir)
+        super(EB_Lua, self).install_step()
