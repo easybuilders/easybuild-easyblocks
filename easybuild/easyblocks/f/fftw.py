f356585a8efec54710b78b03851ab6bbefacebd2
Bart Oldeman <bart.oldeman@calculquebec.ca>
2018-05-04 12:38:57 +0000

    FFTW: proper fix for avx512.

--- easybuild/easyblocks/f/fftw.py
+++ easybuild/easyblocks/f/fftw.py
@@ -40,7 +40,7 @@ from easybuild.tools.toolchain.compiler import OPTARCH_GENERIC
 
 
 # AVX*, FMA4 (AMD Bulldozer+ only), SSE2 (x86_64 only)
-FFTW_CPU_FEATURE_FLAGS_SINGLE_DOUBLE = ['avx', 'avx2', 'avx512f', 'fma4', 'sse2', 'vsx']
+FFTW_CPU_FEATURE_FLAGS_SINGLE_DOUBLE = ['avx', 'avx2', 'avx512', 'fma4', 'sse2', 'vsx']
 # Altivec (POWER), SSE (x86), NEON (ARM), FMA (x86_64)
 # asimd is CPU feature for extended NEON on AARCH64
 FFTW_CPU_FEATURE_FLAGS = FFTW_CPU_FEATURE_FLAGS_SINGLE_DOUBLE + ['altivec', 'asimd', 'neon', 'sse']
@@ -70,8 +70,6 @@ class EB_FFTW(ConfigureMake):
         for flag in FFTW_CPU_FEATURE_FLAGS:
             if flag == 'fma4':
                 conf_opt = 'avx-128-fma'
-            elif flag == 'avx512f':
-                conf_opt = 'avx512'
             else:
                 conf_opt = flag
 
@@ -124,6 +122,9 @@ class EB_FFTW(ConfigureMake):
             # on macOS, AVX is indicated with 'avx1.0' rather than 'avx'
             if 'avx1.0' in avail_cpu_features:
                 avail_cpu_features.append('avx')
+            # avx512 availability is indicated via avx512f and other features starting with avx512
+            if 'axv512f' in avail_cpu_features:
+                avail_cpu_features.append('avx512')
 
             self.log.info("List of available CPU features: %s", avail_cpu_features)
 
