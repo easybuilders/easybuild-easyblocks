1c88faea11f6ddd2d9c990409927186e8f39604c
Kenneth Hoste <kenneth.hoste@ugent.be>
2018-05-22 10:41:37 +0200

    only let generated module file for PGI set license environment variable when it's actually defined

--- easybuild/easyblocks/p/pgi.py
+++ easybuild/easyblocks/p/pgi.py
@@ -177,7 +177,8 @@ class EB_PGI(PackedBinary):
     def make_module_extra(self):
         """Add environment variables LM_LICENSE_FILE and PGI for license file and PGI location"""
         txt = super(EB_PGI, self).make_module_extra()
-        txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file],
-                                                   allow_abs=True, expand_relpaths=False)
+        if self.license_env_var:
+            txt += self.module_generator.prepend_paths(self.license_env_var, [self.license_file],
+                                                       allow_abs=True, expand_relpaths=False)
         txt += self.module_generator.set_environment('PGI', self.installdir)
         return txt
