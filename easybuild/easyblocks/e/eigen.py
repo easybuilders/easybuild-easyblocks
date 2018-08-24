175326cc90d956c38bbd6fcb14b3205f0ee3b594
Kenneth Hoste <kenneth.hoste@ugent.be>
2018-08-24 17:15:09 +0200

    add comment clarifying version check for CMake-based installation of Eigen

--- easybuild/easyblocks/e/eigen.py
+++ easybuild/easyblocks/e/eigen.py
@@ -33,6 +33,9 @@ class EB_Eigen(CMakeMake):
 
     def configure_step(self):
         """Custom configuration procedure for Eigen."""
+        # start using CMake for Eigen 3.3.4 and newer versions
+        # not done for older versions, since this implies using (a dummy-built) CMake as a build dependency,
+        # which is a bit strange for a header-only library like Eigen...
         if LooseVersion(self.version) >= LooseVersion('3.3.4'):
             self.cfg['separate_build_dir'] = True
             # avoid that include files are installed into include/eigen3/Eigen, should be include/Eigen
