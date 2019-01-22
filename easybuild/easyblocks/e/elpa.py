e5199c86036d5ad638d7663bc35c78e947852abf
Micael Oliveira <micael.oliveira@mpsd.mpg.de>
2019-01-22 16:19:52 +0100

    Delete unused import.

--- easybuild/easyblocks/e/elpa.py
+++ easybuild/easyblocks/e/elpa.py
@@ -36,7 +36,7 @@ from easybuild.easyblocks.generic.configuremake import ConfigureMake
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.config import build_option
-from easybuild.tools.systemtools import get_cpu_architecture, get_cpu_features, get_shared_lib_ext
+from easybuild.tools.systemtools import get_cpu_features, get_shared_lib_ext
 from easybuild.tools.toolchain.compiler import OPTARCH_GENERIC
 ELPA_CPU_FEATURE_FLAGS = ['avx', 'avx2', 'avx512', 'vsx', 'sse4_2']
 
