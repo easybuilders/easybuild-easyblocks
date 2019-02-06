4e3bedc102b744be8d4c2038243396b3edf0b198
Bob <b.e.droge@rug.nl>
2019-02-06 12:07:27 +0100

    Use go get instead of go get -d + go build + go install

--- easybuild/easyblocks/generic/gopackage.py
+++ easybuild/easyblocks/generic/gopackage.py
@@ -31,44 +31,25 @@ EasyBuild support for building and installing Go packages, implemented as an eas
 import os
 import shutil
 
-from easybuild.framework.extensioneasyblock import ExtensionEasyBlock
+from easybuild.easyblocks.generic.tarball import Tarball
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.run import run_cmd
 from easybuild.tools.modules import get_software_root
 
-class GoPackage(ExtensionEasyBlock):
+class GoPackage(Tarball):
     """
     Install a Go package as a separate module, or as an extension.
     """
 
-    def configure_step(self):
-        """Custom configure: fetch the sources of the dependencies."""
-        if not get_software_root("Go"):
-            raise EasyBuildError("Go packages require to have a Go module in the builddependencies.")
-        cmd = "env GOPATH=%s go get -d ./..." % (self.builddir)
-        run_cmd(cmd)
-
-    def build_step(self):
-        """Build the Go packages."""
-        cmd = "env GOPATH=%s go build ./..." % (self.builddir)
-        run_cmd(cmd)
-
     def install_step(self):
         """Install procedure for Go packages."""
-        cmd = "env GOBIN=%s GOPATH=%s go install ./..." % (self.installdir, self.builddir)
+        if not get_software_root("Go"):
+            raise EasyBuildError("Go packages require to have a Go module in the builddependencies.")
+        cmd = "env GOBIN=%s GOPATH=%s go get ./..." % (self.installdir, self.builddir)
         run_cmd(cmd)
 
     def make_module_req_guess(self):
         """Add the installation directory to PATH."""
         guesses = super(GoPackage, self).make_module_req_guess()
         guesses['PATH'] = ['']
-        return guesses
-
-    def make_module_req_guess(self):
-        """Customized dictionary of paths to look for with PERL*LIB."""
-
-        guesses = super(GoPackage, self).make_module_req_guess()
-        guesses.update({
-            "PATH" : [''],
-        })
         return guesses
