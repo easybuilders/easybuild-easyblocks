228d89973add6d7f4cf4ac39f8f48af3fe36ecf3
Davide Vanzo <vanzo.davide@gmail.com>
2019-05-01 15:50:31 -0500

    Added prebuildopts to build command line

--- easybuild/easyblocks/c/charmm.py
+++ easybuild/easyblocks/c/charmm.py
@@ -110,7 +110,7 @@ class EB_CHARMM(EasyBlock):
         if self.toolchain.comp_family() == toolchain.INTELCOMP:
             build_options += " IFORT"
 
-        cmd = "./install.com %s %s %s" % (self.arch, self.cfg['system_size'], build_options)
+        cmd = "%s ./install.com %s %s %s" % (self.cfg['prebuildopts'], self.arch, self.cfg['system_size'], build_options)
         (out, _) = run_cmd(cmd, log_all=True, simple=False, log_output=verbose)
         return out
 
