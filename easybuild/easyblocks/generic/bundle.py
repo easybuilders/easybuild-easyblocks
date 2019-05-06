174973d6de400f71e575ecc3e7d3f72136b87df2
Alan O'Cais <a.ocais@fz-juelich.de>
2019-05-06 15:20:31 +0200

    Need to update list with list

--- easybuild/easyblocks/generic/bundle.py
+++ easybuild/easyblocks/generic/bundle.py
@@ -129,7 +129,7 @@ class Bundle(EasyBlock):
                             # Update source_urls in the 'source' dict to use the one for the components
                             # (if it doesn't already exist)
                             source['source_urls'] = source.get('source_urls', cfg['source_urls'])
-                            self.cfg.update('sources', source)
+                            self.cfg.update('sources', [source])
                         else:
                             raise EasyBuildError("Source %s for component %s is neither a string nor a dict, cannot "
                                                  "process it.", source, cfg['name'])
