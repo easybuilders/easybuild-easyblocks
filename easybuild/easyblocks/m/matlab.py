9f3db009d642bdb6aad11cc099467ecafaa032c6
Samuel Moors <samuel.moors@vub.be>
2019-05-07 10:37:20 +0200

    make suggested change

--- easybuild/easyblocks/m/matlab.py
+++ easybuild/easyblocks/m/matlab.py
@@ -68,14 +68,14 @@ class EB_MATLAB(PackedBinary):
     def configure_step(self):
         """Configure MATLAB installation: create license file."""
 
-        licserv = self.cfg['license_server']
-        if licserv is None:
-            licserv = os.getenv('EB_MATLAB_LICENSE_SERVER', 'license.example.com')
-        licport = self.cfg['license_server_port']
-        if licport is None:
-            licport = os.getenv('EB_MATLAB_LICENSE_SERVER_PORT', '00000')
         licfile = self.cfg['license_file']
         if licfile is None:
+            licserv = self.cfg['license_server']
+            if licserv is None:
+                licserv = os.getenv('EB_MATLAB_LICENSE_SERVER', 'license.example.com')
+            licport = self.cfg['license_server_port']
+            if licport is None:
+                licport = os.getenv('EB_MATLAB_LICENSE_SERVER_PORT', '00000')
             # create license file
             lictxt = '\n'.join([
                 "SERVER %s 000000000000 %s" % (licserv, licport),
