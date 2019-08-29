e20a4fc0558b72ee05da0ba3b7cb22e9b91d6604
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-08-29 09:37:27 +0200

    style cleanup in setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ or
 """
 
 import os
-import re
 import sys
 from distutils import log
 from distutils.core import setup
@@ -43,29 +42,31 @@ from easyblocks import VERSION
 
 FRAMEWORK_MAJVER = str(VERSION).split('.')[0]
 
-# log levels: 0 = WARN (default), 1 = INFO, 2 = DEBUG
+# log levels: 0=WARN (default), 1=INFO, 2=DEBUG
 log.set_verbosity(1)
+
 
 # Utility function to read README file
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 log.info("Installing version %s (required versions: API >= %s)" % (VERSION, FRAMEWORK_MAJVER))
 
 setup(
-    name = "easybuild-easyblocks",
-    version = str(VERSION),
-    author = "EasyBuild community",
-    author_email = "easybuild@lists.ugent.be",
-    description = """Python modules which implement support for installing particular (groups of) software packages with EasyBuild.""",
-    license = "GPLv2",
-    keywords = "software build building installation installing compilation HPC scientific",
-    url = "https://easybuilders.github.io/easybuild",
-    packages = ["easybuild", "easybuild.easyblocks", "easybuild.easyblocks.generic"],
-    package_dir = {"easybuild.easyblocks": "easybuild/easyblocks"},
-    package_data = {'easybuild.easyblocks': ["[a-z0-9]/*.py"]},
-    long_description = read("README.rst"),
-    classifiers = [
+    name="easybuild-easyblocks",
+    version=str(VERSION),
+    author="EasyBuild community",
+    author_email="easybuild@lists.ugent.be",
+    description="""Python modules which implement support for installing particular (groups of) software packages with EasyBuild.""",
+    license="GPLv2",
+    keywords="software build building installation installing compilation HPC scientific",
+    url="https://easybuilders.github.io/easybuild",
+    packages=["easybuild", "easybuild.easyblocks", "easybuild.easyblocks.generic"],
+    package_dir={"easybuild.easyblocks": "easybuild/easyblocks"},
+    package_data={'easybuild.easyblocks': ["[a-z0-9]/*.py"]},
+    long_description=read("README.rst"),
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: System Administrators",
@@ -78,6 +79,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Build Tools",
     ],
-    platforms = "Linux",
+    platforms="Linux",
     requires=["easybuild_framework(>=%s.0)" % FRAMEWORK_MAJVER],
 )
