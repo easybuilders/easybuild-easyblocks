e46680f70c9de4e1bb16c1be982ab44faad1492d
darkless <darkless69@gmail.com>
2019-10-14 12:35:20 +0200

    Added CMakeNinja easyblock, which is needed for building Qt5Webkit.

---
+++ easybuild/easyblocks/generic/cmakeninja.py
@@ -0,0 +1,23 @@
+# Author: Pavel Grochal (INUITS)
+# License: GPLv2
+
+"""
+EasyBuild mix of CMake configure step and Ninja build install.
+@author: Pavel Grochal (INUITS)
+"""
+from easybuild.easyblocks.generic.cmakemake import CMakeMake
+from easybuild.easyblocks.generic.mesonninja import MesonNinja
+
+
+class CMakeNinja(CMakeMake, MesonNinja):
+
+    def configure_step(self, *args, **kwargs):
+        CMakeMake.configure_step(self, *args, **kwargs)
+
+    def build_step(self, *args, **kwargs):
+        MesonNinja.build_step(self, *args, **kwargs)
+
+    def install_step(self, *args, **kwargs):
+        MesonNinja.install_step(self, *args, **kwargs)
+
+#TODO: sanity_check_step
