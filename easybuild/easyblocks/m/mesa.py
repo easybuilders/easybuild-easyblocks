3c02f7574705673574c0dfaed06bd1bd662a54c3
edmondac-admin <a.c.edmondson@bham.ac.uk>
2019-12-18 15:00:11 +0000

    Change options based on arch for Mesa

---
+++ easybuild/easyblocks/m/mesa.py
@@ -0,0 +1,17 @@
+# This easyblock was created by the BEAR Software team at the University of Birmingham
+
+from easybuild.easyblocks.generic.mesonninja import MesonNinja
+from easybuild.tools.systemtools import POWER, X86_64, get_cpu_architecture
+
+
+class EB_Mesa(MesonNinja):
+    def configure_step(self, cmd_prefix=''):
+        """
+        Customise the configopts based on the platform
+        """
+        arch = get_cpu_architecture()
+        if arch == X86_64:
+            self.cfg.update('configopts', "-Dgallium-drivers='swrast,swr' -Dswr-arches=avx,avx2,skx,knl")
+        elif arch == POWER:
+            self.cfg.update('configopts', "-Dgallium-drivers='swrast'")
+        return super(EB_Mesa, self).configure_step(cmd_prefix=cmd_prefix)
