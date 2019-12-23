cb0e6107fbb3e30b15af629f80d67a2583384346
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-12-23 14:51:40 +0100

    clean up check for libfabric source tarball in impi easyblock

--- easybuild/easyblocks/i/impi.py
+++ easybuild/easyblocks/i/impi.py
@@ -128,11 +128,11 @@ EULA=accept
         # recompile libfabric (if requested)
         # some Intel MPI versions (like 2019 update 6) no longer ship libfabric sources
         libfabric_path = os.path.join(self.installdir, 'libfabric')
-        if (impiver >= LooseVersion('2019') and self.cfg['libfabric_rebuild'] and
-                os.path.exists(os.path.join(libfabric_path, 'src.tgz'))):
-            if self.cfg['ofi_internal']:
+        if impiver >= LooseVersion('2019') and self.cfg['libfabric_rebuild']:
+            libfabric_src_tgz_fn = 'src.tgz'
+            if self.cfg['ofi_internal'] and os.path.exists(os.path.join(libfabric_path, libfabric_src_tgz_fn)):
                 change_dir(libfabric_path)
-                extract_file('src.tgz', os.getcwd())
+                extract_file(libfabric_src_tgz_fn, os.getcwd())
                 libfabric_installpath = os.path.join(self.installdir, 'intel64', 'libfabric')
 
                 make = 'make'
