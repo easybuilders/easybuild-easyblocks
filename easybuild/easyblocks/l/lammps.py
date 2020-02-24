8bbd477bd71b7fd1a2e48ce0df31bcdffb33faa3
darkless <darkless69@gmail.com>
2020-02-24 10:22:44 +0100

    Fixed remarks.

--- easybuild/easyblocks/l/lammps.py
+++ easybuild/easyblocks/l/lammps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ##
 # Copyright 2009-2020 Ghent University
 #
@@ -25,10 +26,7 @@
 """
 @author: Pavel Grochal (INUITS)
 @author: Kenneth Hoste (Ghent University)
-@author: Alan O'Cais
-
-Based on work of Alan O'Cais (https://github.com/ocaisa)
-https://github.com/easybuilders/easybuild-easyconfigs/blob/0c7fa07b9b7a855df6d14b971bd9eb1a25f51dd8/easybuild/easyconfigs/l/LAMMPS/LAMMPS-24Oct2018-intel-2018b.eb
+@author: Alan O'Cais (Juelich Supercomputing Centre)
 """
 
 import os
@@ -71,18 +69,18 @@ KOKKOS_CPU_MAPPING = {
 
 
 KOKKOS_GPU_ARCH_TABLE = {
-    "3.0": "Kepler30",  # NVIDIA Kepler generation CC 3.0
-    "3.2": "Kepler32",  # NVIDIA Kepler generation CC 3.2
-    "3.5": "Kepler35",  # NVIDIA Kepler generation CC 3.5
-    "3.7": "Kepler37",  # NVIDIA Kepler generation CC 3.7
-    "5.0": "Maxwell50",  # NVIDIA Maxwell generation CC 5.0
-    "5.2": "Maxwell52",  # NVIDIA Maxwell generation CC 5.2
-    "5.3": "Maxwell53",  # NVIDIA Maxwell generation CC 5.3
-    "6.0": "Pascal60",  # NVIDIA Pascal generation CC 6.0
-    "6.1": "Pascal61",  # NVIDIA Pascal generation CC 6.1
-    "7.0": "Volta70",  # NVIDIA Volta generation CC 7.0
-    "7.2": "Volta72",  # NVIDIA Volta generation CC 7.2
-    "7.5": "Turing75",  # NVIDIA Turing generation CC 7.5
+    '3.0': 'Kepler30',  # NVIDIA Kepler generation CC 3.0
+    '3.2': 'Kepler32',  # NVIDIA Kepler generation CC 3.2
+    '3.5': 'Kepler35',  # NVIDIA Kepler generation CC 3.5
+    '3.7': 'Kepler37',  # NVIDIA Kepler generation CC 3.7
+    '5.0': 'Maxwell50',  # NVIDIA Maxwell generation CC 5.0
+    '5.2': 'Maxwell52',  # NVIDIA Maxwell generation CC 5.2
+    '5.3': 'Maxwell53',  # NVIDIA Maxwell generation CC 5.3
+    '6.0': 'Pascal60',  # NVIDIA Pascal generation CC 6.0
+    '6.1': 'Pascal61',  # NVIDIA Pascal generation CC 6.1
+    '7.0': 'Volta70',  # NVIDIA Volta generation CC 7.0
+    '7.2': 'Volta72',  # NVIDIA Volta generation CC 7.2
+    '7.5': 'Turing75',  # NVIDIA Turing generation CC 7.5
 }
 
 
@@ -106,6 +104,7 @@ class EB_LAMMPS(CMakeMake):
         return CMakeMake.extra_options(extra_vars)
 
     def prepare_step(self, *args, **kwargs):
+        """Custom prepare step for LAMMPS."""
         super(EB_LAMMPS, self).prepare_step(*args, **kwargs)
 
         # Unset LIBS when using both KOKKOS and CUDA - it will mix lib paths otherwise
@@ -121,7 +120,7 @@ class EB_LAMMPS(CMakeMake):
         # (2) in the EasyBuild configuration, via --cuda-compute-capabilities configuration option;
         ec_cuda_cc = self.cfg['cuda_compute_capabilities']
         cfg_cuda_cc = build_option('cuda_compute_capabilities')
-        cuda_cc = cfg_cuda_cc or ec_cuda_cc or []
+        cuda_cc = check_cuda_compute_capabilities(cfg_cuda_cc, ec_cuda_cc)
 
         # cmake has its own folder
         self.cfg['separate_build_dir'] = True
@@ -205,11 +204,10 @@ class EB_LAMMPS(CMakeMake):
                 self.cfg.update('configopts', '-DKOKKOS_ENABLE_OPENMP=yes')
 
             self.cfg.update('configopts', '-DPKG_KOKKOS=on')
-            self.cfg.update('configopts', '-DKOKKOS_ARCH="%s"' % self.get_kokkos_arch(cuda_cc))
+            self.cfg.update('configopts', '-DKOKKOS_ARCH="%s"' % get_kokkos_arch(cuda_cc, self.cfg['kokkos_arch']))
 
             # if KOKKOS and CUDA
             if cuda:
-                self.check_cuda_compute_capabilities(cfg_cuda_cc, ec_cuda_cc, cuda_cc)
                 nvcc_wrapper_path = os.path.join(self.start_dir, "lib", "kokkos", "bin", "nvcc_wrapper")
                 self.cfg.update('configopts', '-DKOKKOS_ENABLE_CUDA=yes')
                 self.cfg.update('configopts', '-DCMAKE_CXX_COMPILER="%s"' % nvcc_wrapper_path)
@@ -219,9 +217,7 @@ class EB_LAMMPS(CMakeMake):
         elif cuda:
             self.cfg.update('configopts', '-DPKG_GPU=on')
             self.cfg.update('configopts', '-DGPU_API=cuda')
-
-            self.check_cuda_compute_capabilities(cfg_cuda_cc, ec_cuda_cc, cuda_cc)
-            self.cfg.update('configopts', '-DGPU_ARCH=%s' % self.get_cuda_gpu_arch(cuda_cc))
+            self.cfg.update('configopts', '-DGPU_ARCH=%s' % get_cuda_gpu_arch(cuda_cc))
 
         # avoid that pip (ab)uses $HOME/.cache/pip
         # cfr. https://pip.pypa.io/en/stable/reference/pip_install/#caching
@@ -231,6 +227,7 @@ class EB_LAMMPS(CMakeMake):
         return super(EB_LAMMPS, self).configure_step()
 
     def sanity_check_step(self, *args, **kwargs):
+        """Run custom sanity checks for LAMMPS files, dirs and commands."""
         check_files = [
             'atm', 'balance', 'colloid', 'crack', 'dipole', 'friction',
             'hugoniostat', 'indent', 'melt', 'message', 'min', 'msst',
@@ -280,87 +277,108 @@ class EB_LAMMPS(CMakeMake):
 
         return txt
 
-    def get_cuda_gpu_arch(self, cuda_cc):
-        """Return CUDA gpu ARCH in LAMMPS required format (eg. sm_32)"""
-        # Get largest cuda supported
-        return 'sm_%s' % str(cuda_cc.sorted(reverse=True)[0]).replace(".", "")
 
-    def get_kokkos_arch(self, cuda_cc):
-        """
-        Return KOKKOS ARCH in LAMMPS required format
+def get_cuda_gpu_arch(cuda_cc):
+    """Return CUDA gpu ARCH in LAMMPS required format. Example: 'sm_32' """
+    # Get largest cuda supported
+    return 'sm_%s' % str(cuda_cc.sorted(reverse=True)[0]).replace(".", "")
 
-        see: https://lammps.sandia.gov/doc/Build_extras.html#kokkos
-        """
-        cuda = get_software_root('CUDA')
-        processor_arch = None
 
-        if self.cfg['kokkos_arch']:
-            if self.cfg['kokkos_arch'] not in KOKKOS_CPU_ARCH_LIST:
-                warning_msg = "Specified CPU ARCH (%s) " % self.cfg['kokkos_arch']
-                warning_msg += "was not found in listed options [%s]." % KOKKOS_CPU_ARCH_LIST
-                warning_msg += "Still might work though."
-                print_warning(warning_msg)
-            processor_arch = self.cfg['kokkos_arch']
+def get_kokkos_arch(cuda_cc, kokkos_arch):
+    """
+    Return KOKKOS ARCH in LAMMPS required format, which is either 'CPU_ARCH' or 'CPU_ARCH;GPU_ARCH'.
 
-        else:
-            warning_msg = "kokkos_arch not set. Trying to auto-detect CPU arch."
+    see: https://lammps.sandia.gov/doc/Build_extras.html#kokkos
+    """
+    cuda = get_software_root('CUDA')
+    processor_arch = None
+
+    if kokkos_arch:
+        if kokkos_arch not in KOKKOS_CPU_ARCH_LIST:
+            warning_msg = "Specified CPU ARCH (%s) " % kokkos_arch
+            warning_msg += "was not found in listed options [%s]." % KOKKOS_CPU_ARCH_LIST
+            warning_msg += "Still might work though."
             print_warning(warning_msg)
+        processor_arch = kokkos_arch
 
-            processor_arch = KOKKOS_CPU_MAPPING.get(self.get_cpu_arch())
+    else:
+        warning_msg = "kokkos_arch not set. Trying to auto-detect CPU arch."
+        print_warning(warning_msg)
 
-            if not processor_arch:
-                error_msg = "Couldn't determine CPU architecture, you need to set 'kokkos_arch' manually."
-                raise EasyBuildError(error_msg)
+        processor_arch = KOKKOS_CPU_MAPPING.get(get_cpu_arch())
 
-            print_msg("Determined cpu arch: %s" % processor_arch)
+        if not processor_arch:
+            error_msg = "Couldn't determine CPU architecture, you need to set 'kokkos_arch' manually."
+            raise EasyBuildError(error_msg)
 
-        if cuda:
-            # CUDA below
-            gpu_arch = None
-            for cc in cuda_cc.sorted(reverse=True):
-                gpu_arch = KOKKOS_GPU_ARCH_TABLE.get(str(cc))
-                if gpu_arch:
-                    break
-                else:
-                    warning_msg = "(%s) GPU ARCH was not found in listed options." % cc
-                    print_warning(warning_msg)
+        print_msg("Determined cpu arch: %s" % processor_arch)
 
-            if not gpu_arch:
-                error_msg = "Specified GPU ARCH (%s) " % cuda_cc
-                error_msg += "was not found in listed options [%s]." % KOKKOS_GPU_ARCH_TABLE
-                raise EasyBuildError(error_msg)
-
-            kokkos_arch = "%s;%s" % (processor_arch, gpu_arch)
-
-        else:
-            kokkos_arch = processor_arch
-
-        return kokkos_arch
-
-    def check_cuda_compute_capabilities(self, cfg_cuda_cc, ec_cuda_cc, cuda_cc):
-        cuda = get_software_root('CUDA')
-
-        if cuda:
-            if cfg_cuda_cc and ec_cuda_cc:
-                warning_msg = "cuda_compute_capabilities specified in easyconfig (%s)" % self.ec_cuda_cc
-                warning_msg += " are overruled by "
-                warning_msg += "--cuda-compute-capabilities configuration option (%s)" % self.cfg_cuda_cc
+    if cuda:
+        # CUDA below
+        gpu_arch = None
+        for cc in cuda_cc.sorted(reverse=True):
+            gpu_arch = KOKKOS_GPU_ARCH_TABLE.get(str(cc))
+            if gpu_arch:
+                break
+            else:
+                warning_msg = "(%s) GPU ARCH was not found in listed options." % cc
                 print_warning(warning_msg)
-            elif not cuda_cc:
-                error_msg = "No CUDA compute capabilities specified.\nTo build LAMMPS with Cuda you need to use"
-                error_msg += "the --cuda-compute-capabilities configuration option or the cuda_compute_capabilities "
-                error_msg += "easyconfig parameter to specify a list of CUDA compute capabilities to compile with."
-                raise EasyBuildError(error_msg)
 
-        elif cuda_cc:
-            warning_msg = "Missing CUDA package (in dependencies), "
-            warning_msg += "but 'cuda_compute_capabilities' option was specified."
+        if not gpu_arch:
+            error_msg = "Specified GPU ARCH (%s) " % cuda_cc
+            error_msg += "was not found in listed options [%s]." % KOKKOS_GPU_ARCH_TABLE
+            raise EasyBuildError(error_msg)
+
+        kokkos_arch = "%s;%s" % (processor_arch, gpu_arch)
+
+    else:
+        kokkos_arch = processor_arch
+
+    return kokkos_arch
+
+
+def check_cuda_compute_capabilities(cfg_cuda_cc, ec_cuda_cc):
+    """
+    Checks if cuda-compute-capabilities is set and prints warning if it gets declared on multiple places.
+
+    :param cfg_cuda_cc: cuda-compute-capabilities from cli config
+    :param ec_cuda_cc: cuda-compute-capabilities from easyconfig
+    :return: returns preferred cuda-compute-capabilities
+    """
+
+    cuda = get_software_root('CUDA')
+    cuda_cc = cfg_cuda_cc or ec_cuda_cc or []
+
+    if cuda:
+        if cfg_cuda_cc and ec_cuda_cc:
+            warning_msg = "cuda_compute_capabilities specified in easyconfig (%s)" % ec_cuda_cc
+            warning_msg += " are overruled by "
+            warning_msg += "--cuda-compute-capabilities configuration option (%s)" % cfg_cuda_cc
             print_warning(warning_msg)
+        elif not cuda_cc:
+            error_msg = "No CUDA compute capabilities specified.\nTo build LAMMPS with Cuda you need to use"
+            error_msg += "the --cuda-compute-capabilities configuration option or the cuda_compute_capabilities "
+            error_msg += "easyconfig parameter to specify a list of CUDA compute capabilities to compile with."
+            raise EasyBuildError(error_msg)
 
-        return cuda_cc
+    elif cuda_cc:
+        warning_msg = "Missing CUDA package (in dependencies), "
+        warning_msg += "but 'cuda_compute_capabilities' option was specified."
+        print_warning(warning_msg)
 
-    def get_cpu_arch(self):
-        out, ec = run_cmd("python -c 'from archspec.cpu import host; print(host())'", simple=False)
-        if ec:
-            raise EasyBuildError("Failed to determine CPU architecture: %s", out)
-        return out.strip()
+    return cuda_cc
+
+
+def get_cpu_arch():
+    """
+    Checks for CPU architecture via archspec library.
+    https://github.com/archspec/archspec
+    Archspec should be bundled as build-dependency to determine CPU arch.
+    It can't be called directly in code because it gets available only after prepare_step.
+
+    :return: returns detected cpu architecture
+    """
+    out, ec = run_cmd("python -c 'from archspec.cpu import host; print(host())'", simple=False)
+    if ec:
+        raise EasyBuildError("Failed to determine CPU architecture: %s", out)
+    return out.strip()
