ad1eb76c42c3ecbbebdc4546fbffaddd8784f65c
darkless <darkless69@gmail.com>
2020-03-29 15:27:30 +0200

    fixed remarks

--- easybuild/easyblocks/s/sepp.py
+++ easybuild/easyblocks/s/sepp.py
@@ -28,44 +28,62 @@ EasyBuild support for building and installing SEPP, implemented as an easyblock
 """
 import os
 
-from easybuild.tools.filetools import copy_dir, write_file, apply_regex_substitutions
-from easybuild.tools.run import run_cmd
-
 from easybuild.easyblocks.generic.pythonpackage import PythonPackage
+from easybuild.tools.filetools import apply_regex_substitutions, copy_dir, write_file
+from easybuild.tools.run import run_cmd
 
 
 class EB_SEPP(PythonPackage):
     """Support for installing the SEPP Python package as part of a Python installation."""
 
     def configure_step(self, *args, **kwargs):
-        """Build sepp using setup.py."""
+        """Configure SEPP using setup.py."""
         super(EB_SEPP, self).configure_step(*args, **kwargs)
 
         # Configure sepp
         run_cmd("python setup.py config -c")
 
     def install_step(self, *args, **kwargs):
+        """
+        Create required SEPP files:
+        home.path - file specifying path to SEPP config dir (.sepp)
+        main.config - SEPP configuration file
+        """
         super(EB_SEPP, self).install_step(*args, **kwargs)
 
         python_site_packages_dir = os.path.join(self.installdir, self.pylibdir)
 
-        # Old (wrong) SEPP paths
-        sepp_wrong_config_dir = os.path.join(self.builddir, self.name.lower() + "-" + self.version, '.sepp')
+        # original path to SEPP config
+        sepp_orig_config_dir = os.path.join(self.builddir, self.name.lower() + "-" + self.version, '.sepp')
 
-        # New (correct) SEPP paths
-        sepp_correct_home_path_file = os.path.join(python_site_packages_dir, 'home.path')
-        sepp_correct_config_dir = os.path.join(python_site_packages_dir, '.sepp')
-        sepp_correct_config_file = os.path.join(sepp_correct_config_dir, 'main.config')
+        # correct SEPP paths
+        sepp_final_home_path_file = os.path.join(python_site_packages_dir, 'home.path')
+        sepp_final_config_dir = os.path.join(python_site_packages_dir, '.sepp')
+        sepp_final_config_file = os.path.join(sepp_final_config_dir, 'main.config')
 
-        # Create correct home.path file
-        self.log.info("Creating home.path file for SEPP at %s", sepp_correct_home_path_file)
-        write_file(sepp_correct_home_path_file, sepp_correct_config_dir)
+        # create correct home.path file which contains location of .sepp config dir
+        self.log.info("Creating home.path file for SEPP at %s", sepp_final_home_path_file)
+        write_file(sepp_final_home_path_file, sepp_final_config_dir)
 
         # Copy .sepp folder with configurations and bundled stuff from builddir to installdir
-        copy_dir(sepp_wrong_config_dir, sepp_correct_config_dir)
+        copy_dir(sepp_orig_config_dir, sepp_final_config_dir)
 
         # Replace wrong paths in SEPP configfile with correct ones
         regex_subs = [
-            (r'%s' % sepp_wrong_config_dir, '%s' % sepp_correct_config_dir),
+            (r'%s' % sepp_orig_config_dir, '%s' % sepp_final_config_dir),
         ]
-        apply_regex_substitutions(sepp_correct_config_file, regex_subs)
+        apply_regex_substitutions(sepp_final_config_file, regex_subs)
+
+    def sanity_check_step(self):
+        """Custom sanity check for SEPP."""
+        scripts = [
+            'run_abundance.py', 'run_sepp.py', 'run_tipp.py',
+            'run_tipp_tool.py', 'run_upp.py', 'split_sequences.py'
+        ]
+        custom_paths = {
+            'files': [os.path.join('bin', s) for s in scripts],
+            'dirs': [os.path.join(self.pylibdir, 'sepp')],
+        }
+        custom_commands = ["%s --help" % s for s in scripts]
+
+        super(EB_SEPP, self).sanity_check_step(custom_paths=custom_paths, custom_commands=custom_commands)
