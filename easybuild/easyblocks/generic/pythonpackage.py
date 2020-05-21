da2bb4e63f9ff03415a676b014a9eaae0a70c349
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2020-05-21 16:12:58 +0000

    added option use_pip_requirement to allow providing a requirements file to the pip command, as a source

--- easybuild/easyblocks/generic/pythonpackage.py
+++ easybuild/easyblocks/generic/pythonpackage.py
@@ -222,6 +222,7 @@ class PythonPackage(ExtensionEasyBlock):
             # see https://packaging.python.org/tutorials/installing-packages/#installing-setuptools-extras
             'use_pip_extras': [None, "String with comma-separated list of 'extras' to install via pip", CUSTOM],
             'use_pip_for_deps': [False, "Install dependencies using '%s'" % PIP_INSTALL_CMD, CUSTOM],
+            'use_pip_requirement': [False, "Install using pip install --requirement <requirements file>", CUSTOM],
             'use_setup_py_develop': [False, "Install using '%s' (deprecated)" % SETUP_PY_DEVELOP_CMD, CUSTOM],
             'zipped_egg': [False, "Install as a zipped eggs (requires use_easy_install)", CUSTOM],
         })
@@ -442,6 +443,10 @@ class PythonPackage(ExtensionEasyBlock):
         if self.cfg.get('use_pip_editable', False):
             # add --editable option when requested, in the right place (i.e. right before the location specification)
             loc = "--editable %s" % loc
+
+        if self.cfg.get('use_pip_requirement', False):
+            # add --requirements option when requested, in the right place (i.e. right before the location specification)
+            loc = "--requirement %s" % loc
 
         cmd.extend([
             self.cfg['preinstallopts'],
