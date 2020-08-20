2ef63e34ca7521fb9675b7624840a0739018a160
Alexander Grund <Alexander.Grund@tu-dresden.de>
2020-08-20 13:20:36 +0200

    Improve itac

--- easybuild/easyblocks/i/itac.py
+++ easybuild/easyblocks/i/itac.py
@@ -69,8 +69,7 @@ class EB_itac(IntelBase):
             if LooseVersion(self.version) >= LooseVersion('9.0'):
                 super(EB_itac, self).move_after_install()
         else:
-            silent = \
-                """
+            silent = """
 [itac]
 INSTALLDIR=%(ins)s
 LICENSEPATH=%(lic)s
