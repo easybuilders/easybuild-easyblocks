85b9a9e9020875695713de25ecef140eb431000d
darkless <darkless69@gmail.com>
2020-11-05 13:47:47 +0100

    fixed long line

--- easybuild/easyblocks/m/metagenome_atlas_bundle.py
+++ easybuild/easyblocks/m/metagenome_atlas_bundle.py
@@ -162,7 +162,8 @@ import xml.etree.cElementTree as ET
 jobid = sys.argv[1]
 
 try:
-    res = subprocess.run("qstat -f -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    res = subprocess.run("qstat -f -x {}".format(jobid), check=True, 
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
     xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
     job_state = xmldoc.findall('.//job_state')[0].text
