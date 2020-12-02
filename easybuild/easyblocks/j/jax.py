d459598767d2ebf36151c9c58d174d36086e3d0e
edmondac-admin <a.c.edmondson@bham.ac.uk>
2020-12-02 21:59:31 +0000

    Possibly working JAX easyblock

--- easybuild/easyblocks/j/jax.py
+++ easybuild/easyblocks/j/jax.py
@@ -5,7 +5,8 @@ EasyBuild support for building and installing JAX, implemented as an easyblock
 """
 from easybuild.framework.easyblock import EasyBlock
 from easybuild.tools.systemtools import POWER, get_cpu_architecture
-from easybuild.tools.modules import get_software_root
+from easybuild.tools.modules import get_software_root, get_software_version
+from easybuild.tools.run import run_cmd
 
 
 class EB_JAX(EasyBlock):
@@ -16,25 +17,22 @@ class EB_JAX(EasyBlock):
         pass
 
     def build_step(self):
-        """No build step for JAX."""
+        """Build JAX"""
         cuda = get_software_root('CUDA')
         cudnn = get_software_root('CuDNN')
         bazel = get_software_root('Bazel')
         binutils = get_software_root('binutils')
-        if self.cfg['prebuildopts'] is None:
+        if not self.cfg['prebuildopts']:
             self.cfg['prebuildopts'] = 'export TF_CUDA_PATHS="{}" '.format(cuda)
             self.cfg['prebuildopts'] += 'GCC_HOST_COMPILER_PREFIX="{}/bin" '.format(binutils)
             # To prevent bazel builds on different hosts/architectures conflicting with each other
             # we'll set HOME, inside which Bazel puts active files (in ~/.cache/bazel/...)
-            self.cfg['prebuildopts'] += 'HOME={}/fake_home && '.format(self.installdir)
+            self.cfg['prebuildopts'] += 'HOME="{}/fake_home" && '.format(self.builddir)
 
-        if self.cfg['build_cmd'] is None:
-            self.cfg['build_cmd'] = 'python build/build.py'
-
-        if self.cfg['buildopts'] is None:
-            self.cfg['buildopts'] = ' --enable_cuda --cuda_path {} '.format(cuda)
-            self.cfg['buildopts'] += '--cudnn_path {} '.format(cudnn)
-            self.cfg['buildopts'] += '--bazel_path {}/bin/bazel '.format(bazel)
+        if not self.cfg['buildopts']:
+            self.cfg['buildopts'] = '--enable_cuda --cuda_path "{}" '.format(cuda)
+            self.cfg['buildopts'] += '--cudnn_path "{}" '.format(cudnn)
+            self.cfg['buildopts'] += '--bazel_path "{}/bin/bazel" '.format(bazel)
             # Tell Bazel to pass PYTHONPATH through to what it's building, so it can find scipy etc.
             self.cfg['buildopts'] += '--bazel_options=--action_env=PYTHONPATH '
             self.cfg['buildopts'] += '--noenable_mkl_dnn '
@@ -43,22 +41,43 @@ class EB_JAX(EasyBlock):
                 self.cfg['buildopts'] += (r'--bazel_options=--per_file_copt=.*cu\.cc.*'
                                           '@-nvcc_options=compiler-options=-mno-float128 ')
 
-    def install_step(self):
-        """Install JAX using install script."""
+        cmd = ' '.join([
+            self.cfg['prebuildopts'],
+            'python build/build.py',
+            self.cfg['buildopts'],
+        ])
 
+        (out, _) = run_cmd(cmd, log_all=True, simple=False)
+
+        return out
+
+    def install_step(self):
+        """Install JAX"""
         if self.cfg['install_cmd'] is None:
             self.cfg['install_cmd'] = '(cd build && pip install --prefix {} .) &&'.format(self.installdir)
             self.cfg['install_cmd'] = 'pip install --prefix {} .'.format(self.installdir)
 
-        raise IOError(self.cfg['modextravars'])
-
         super(EB_JAX, self).install_step()
+
+    def make_module_step(self, fake=False):
+        """Make the module"""
+
+        if 'XLA_FLAGS' not in self.cfg['modextravars']:
+            cuda = get_software_root('CUDA')
+            self.cfg['modextravars']['XLA_FLAGS'] = "--xla_gpu_cuda_data_dir={}/bin".format(cuda)
+
+        if 'PYTHONPATH' not in self.cfg['modextrapaths']:
+            pyshortver = '.'.join(get_software_version('Python').split('.')[:2])
+            self.cfg['modextrapaths']['PYTHONPATH'] = 'lib/python{}/site-packages'.format(pyshortver)
+
+        return super(EB_JAX, self).make_module_step(fake)
 
     def sanity_check_step(self):
         """Custom sanity check for JAX."""
+        pyshortver = '.'.join(get_software_version('Python').split('.')[:2])
         custom_paths = {
             'files': [],
-            'dirs': ['lib/python{}/site-packages/jax'.format(self.pyshortver),
-                     'lib/python{}/site-packages/jaxlib'.format(self.pyshortver)],
+            'dirs': ['lib/python{}/site-packages/jax'.format(pyshortver),
+                     'lib/python{}/site-packages/jaxlib'.format(pyshortver)],
         }
         super(EB_JAX, self).sanity_check_step(custom_paths=custom_paths)
