83595884c557189c38a7a0a23a4d01f6e9d9959c
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-16 11:23:49 +0100

    enhance Amber easyblock to fix running of update_amber script when 'python' command is not available in OS

--- easybuild/easyblocks/a/amber.py
+++ easybuild/easyblocks/a/amber.py
@@ -38,6 +38,7 @@ from easybuild.easyblocks.generic.configuremake import ConfigureMake
 from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.filetools import which
 from easybuild.tools.modules import get_software_root
 from easybuild.tools.run import run_cmd
 
@@ -82,8 +83,20 @@ class EB_Amber(ConfigureMake):
     def patch_step(self, *args, **kwargs):
         """Patch Amber using 'update_amber' tool, prior to applying listed patch files (if any)."""
 
+        # figure out which Python command to use to run the update_amber script;
+        # by default it uses 'python', but this may not be available (on CentOS 8 for example);
+        # note that the dependencies are not loaded yet at this point, so we're at the mercy of the OS here...
+        python_cmd = None
+        for cand_python_cmd in ['python', 'python3', 'python2']:
+            if which(cand_python_cmd):
+                python_cmd = cand_python_cmd
+                break
+
+        if python_cmd is None:
+            raise EasyBuildError("No suitable Python command found to run update_amber script!")
+
         if self.cfg['patchlevels'] == "latest":
-            cmd = "./update_amber --update"
+            cmd = "%s ./update_amber --update" % python_cmd
             # Run as many times as specified. It is the responsibility
             # of the easyconfig author to get this right, especially if
             # he or she selects "latest". (Note: "latest" is not
@@ -94,7 +107,7 @@ class EB_Amber(ConfigureMake):
             for (tree, patch_level) in zip(['AmberTools', 'Amber'], self.cfg['patchlevels']):
                 if patch_level == 0:
                     continue
-                cmd = "./update_amber --update-to %s/%s" % (tree, patch_level)
+                cmd = "%s ./update_amber --update-to %s/%s" % (python_cmd, tree, patch_level)
                 # Run as many times as specified. It is the responsibility
                 # of the easyconfig author to get this right.
                 for _ in range(self.cfg['patchruns']):
