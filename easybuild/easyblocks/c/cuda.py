3a14cb16941ba8d0ceab598e165993fbd4532249
Robert Mijakovic <robert.mijakovic@lxp.lu>
2021-03-11 21:25:36 +0000

    Adds the header and adds LuxProvide as one of the contributors.

--- easybuild/easyblocks/c/cuda.py
+++ easybuild/easyblocks/c/cuda.py
@@ -1,14 +1,26 @@
 ##
-# This file is an EasyBuild reciPY as per https://github.com/easybuilders/easybuild
+# Copyright 2012-2021 Ghent University
 #
-# Copyright:: Copyright 2012-2019 Cyprus Institute / CaSToRC, Uni.Lu, NTUA, Ghent University,
-#             Forschungszentrum Juelich GmbH
-# Authors::   George Tsouloupas <g.tsouloupas@cyi.ac.cy>, Fotis Georgatos <fotis@cern.ch>, Kenneth Hoste, Damian Alvarez
-# License::   MIT/GPL
-# $Id$
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
 #
-# This work implements a part of the HPCBIOS project and is a component of the policy:
-# http://hpcbios.readthedocs.org/en/latest/HPCBIOS_2012-99.html
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
 ##
 """
 EasyBuild support for CUDA, implemented as an easyblock
@@ -20,6 +32,7 @@ Ref: https://speakerdeck.com/ajdecon/introduction-to-the-cuda-toolkit-for-buildi
 @author: Kenneth Hoste (Ghent University)
 @author: Damian Alvarez (Forschungszentrum Juelich)
 @author: Ward Poelmans (Free University of Brussels)
+@author: Robert Mijakovic (LuxProvide S.A.)
 """
 import os
 import stat
