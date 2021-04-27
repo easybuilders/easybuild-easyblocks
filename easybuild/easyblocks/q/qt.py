425cb6d2a32b860566d9a28f0a37745af2debca4
Alan O'Cais <a.ocais@fz-juelich.de>
2021-04-27 15:58:58 +0200

    flake8

--- easybuild/easyblocks/q/qt.py
+++ easybuild/easyblocks/q/qt.py
@@ -131,7 +131,7 @@ class EB_Qt(ConfigureMake):
         # * https://gitweb.gentoo.org/proj/qt.git/commit/?id=9ff0752e1ee3c28818197eaaca45545708035152
         kernel_version = os.uname()[2]
         skip_kernel_features = self.cfg['disable_advanced_kernel_features'] or \
-                               LooseVersion(kernel_version) < LooseVersion('3.17')
+            LooseVersion(kernel_version) < LooseVersion('3.17')
         if LooseVersion(self.version) >= LooseVersion('5.10') and skip_kernel_features:
             self.cfg.update('configopts', '-no-feature-renameat2')
             self.cfg.update('configopts', '-no-feature-getentropy')
