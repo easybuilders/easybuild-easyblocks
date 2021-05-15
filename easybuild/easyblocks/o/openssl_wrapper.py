4069cc916cd94b8a41d798ca4269c9c2e4330d80
Alex Domingo <alex.domingo.toro@vub.be>
2021-05-15 17:42:15 +0200

    add support for darwin to openssl_wrapper

--- easybuild/easyblocks/o/openssl_wrapper.py
+++ easybuild/easyblocks/o/openssl_wrapper.py
@@ -28,6 +28,7 @@ EasyBuild support for installing a wrapper module file for OpenSSL
 @author: Alex Domingo (Vrije Universiteit Brussel)
 """
 import ctypes
+import ctypes.macholib.dyld
 import os
 import re
 
@@ -35,8 +36,10 @@ from easybuild.framework.easyblock import EasyBlock
 from easybuild.tools.filetools import expand_glob_paths, find_glob_pattern, mkdir, read_file, symlink, which
 from easybuild.tools.modules import get_software_root
 from easybuild.tools.run import run_cmd
+from easybuild.tools.systemtools import get_shared_lib_ext
+from easybuild.tools.systemtools import DARWIN, LINUX, get_os_type, get_shared_lib_ext
 
-def locate_lib(libobj):
+def locate_solib(libobj):
     """
     Return absolute path to loaded library using dlinfo
     Based on https://stackoverflow.com/a/35683698
@@ -68,23 +71,35 @@ class EB_OpenSSL_wrapper(EasyBlock):
         """Define the names of OpenSSL shared objects"""
         super(EB_OpenSSL_wrapper, self).__init__(*args, **kwargs)
 
-        # Check the system library of OpenSSL
-        self.openssl_libs = ['libssl.so', 'libcrypto.so']
+        # Libraries packaged in OpenSSL
+        self.openssl_libs = ['libssl', 'libcrypto']
 
+        # Check the system library of OpenSSL
         libssl = {
-            '1.0': 'libssl.so.10',
-            '1.1': 'libssl.so.1.1',
+            '1.0': {LINUX: 'libssl.so.10', DARWIN: 'libssl.1.0.dylib'},
+            '1.1': {LINUX: 'libssl.so.1.1', DARWIN: 'libssl.1.1.dylib'},
         }
 
+        os_type = get_os_type()
+
         try:
-            libssl_so = libssl[self.version]
+            libssl_so = libssl[self.version][os_type]
             libssl_obj = ctypes.cdll.LoadLibrary(libssl_so)
         except OSError:
             self.ssl_syslib = None
             self.log.debug("Library '%s' not found in host system", libssl_so)
         else:
-            self.ssl_syslib = locate_lib(libssl_obj)
-            self.log.debug("Found library '%s' in: %s", libssl_so, self.ssl_syslib)
+            # ctypes.util.find_library only accepts unversioned library names
+            if os_type == LINUX:
+                # find path to library with dlinfo
+                self.ssl_syslib = locate_solib(libssl_obj)
+            elif os_type == DARWIN:
+                # ctypes.macholib.dyld.dyld_find accepts file names and returns full path
+                self.ssl_syslib = ctypes.macholib.dyld.dyld_find(libssl_so)
+            else:
+                raise EasyBuildError("Unknown host system type: %s", os_type)
+
+        self.log.debug("Found library '%s' in: %s", libssl_so, self.ssl_syslib)
 
         # Check system include paths for OpenSSL headers
         cmd = "gcc -E -Wp,-v -xc /dev/null"
@@ -129,20 +144,20 @@ class EB_OpenSSL_wrapper(EasyBlock):
 
     def install_step(self):
         """Symlink target OpenSSL installation"""
+        shlib_ext = get_shared_lib_ext()
 
         if self.ssl_syslib and self.ssl_syshead:
             # The host system already provides the necessary OpenSSL files
+            ssl_lib_pattern = ['%s*.%s*' % (lib_so, shlib_ext) for lib_so in self.openssl_libs]
             ssl_lib_pattern = [
-                os.path.join(os.path.dirname(self.ssl_syslib), '%s*' % lib_so) for lib_so in self.openssl_libs
+                os.path.join(os.path.dirname(self.ssl_syslib), '%s' % ptrn) for ptrn in ssl_lib_pattern
             ]
             ssl_include_path = self.ssl_syshead
             ssl_bin = which(self.name.lower())
         else:
             # Use OpenSSL from EasyBuild
             ssl_root = get_software_root(self.name)
-            ssl_lib_pattern = [
-                os.path.join(ssl_root, 'lib', '*.so*'),
-            ]
+            ssl_lib_pattern = [os.path.join(ssl_root, 'lib', '*.%s*' % shlib_ext)]
             ssl_include_path = os.path.join(get_software_root(self.name), 'include', self.name.lower())
             ssl_bin = os.path.join(ssl_root, 'bin', self.name.lower())
 
@@ -171,10 +186,11 @@ class EB_OpenSSL_wrapper(EasyBlock):
 
     def sanity_check_step(self):
         """Custom sanity check for OpenSSL wrapper."""
+        shlib_ext = get_shared_lib_ext()
 
         ssl_bin = os.path.join('bin', self.name.lower())
         ssl_include_dir = os.path.join('include', self.name.lower())
-        ssl_libs = [os.path.join('lib', libso) for libso in self.openssl_libs]
+        ssl_libs = [os.path.join('lib', '%s.%s' % (libso, shlib_ext)) for libso in self.openssl_libs]
 
         custom_paths = {
             'files': [ssl_bin] + ssl_libs,
