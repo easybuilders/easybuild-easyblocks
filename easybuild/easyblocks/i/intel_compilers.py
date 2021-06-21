e05c0f233ac97a187b9e71089ad6320a2ea05165
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2021-06-21 09:10:33 +0200

    Adjust multi-arch include dir to not hardcode /usr/include.

--- easybuild/easyblocks/i/intel_compilers.py
+++ easybuild/easyblocks/i/intel_compilers.py
@@ -135,12 +135,15 @@ class EB_intel_minus_compilers(IntelBase):
         """Additional custom variables for intel-compiler"""
         txt = super(EB_intel_minus_compilers, self).make_module_extra()
 
-        # on Debian/Ubuntu, /usr/include/x86_64-linux-gnu needs to be included in $CPATH for Intel C compiler
-        out, ec = run_cmd("gcc -print-multiarch", simple=False)
-        if ec == 0 and out:
-            multiarch_inc_dir = os.path.join('/usr', 'include', out.strip())
-            self.log.info("Adding multiarch include path %s to $CPATH in generated module file", multiarch_inc_dir)
-            # system location must be appended at the end, so use append_paths
-            txt += self.module_generator.append_paths('CPATH', [multiarch_inc_dir], allow_abs=True)
+        # On Debian/Ubuntu, /usr/include/x86_64-linux-gnu, or whatever dir gcc uses, needs to be included
+        # in $CPATH for Intel C compiler
+        multiarch_out, ec = run_cmd("gcc -print-multiarch", simple=False)
+        if ec == 0 and multiarch_out:
+            multiarch_inc_dir, ec = run_cmd("gcc -E -Wp,-v -xc /dev/null 2>&1 | grep %s$" % multiarch_out.strip())
+            if ec == 0 and multiarch_inc_dir: 
+                multiarch_inc_dir = multiarch_inc_dir.strip()
+                self.log.info("Adding multiarch include path %s to $CPATH in generated module file", multiarch_inc_dir)
+                # system location must be appended at the end, so use append_paths
+                txt += self.module_generator.append_paths('CPATH', [multiarch_inc_dir], allow_abs=True)
 
         return txt
