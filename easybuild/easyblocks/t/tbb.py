d2549beae0c953c5c98c4a0e6f1f0a27dcf2351f
Alexander Grund <Alexander.Grund@tu-dresden.de>
2021-07-13 10:08:29 +0200

    Further cleanup TBB

--- easybuild/easyblocks/t/tbb.py
+++ easybuild/easyblocks/t/tbb.py
@@ -42,6 +42,7 @@ from distutils.version import LooseVersion
 from easybuild.easyblocks.generic.configuremake import ConfigureMake
 from easybuild.easyblocks.generic.intelbase import INSTALL_MODE_NAME_2015, INSTALL_MODE_2015
 from easybuild.easyblocks.generic.intelbase import IntelBase, ACTIVATION_NAME_2012, LICENSE_FILE_NAME_2012
+from easybuild.tools.filetools import symlink
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.modules import get_software_version
 from easybuild.tools.systemtools import POWER, get_cpu_architecture, get_gcc_version, get_platform_name
@@ -77,7 +78,6 @@ class EB_tbb(IntelBase, ConfigureMake):
         """Initialisation of custom class variables for tbb"""
         super(EB_tbb, self).__init__(*args, **kwargs)
 
-        self.libpath = 'UNKNOWN'
         platform_name = get_platform_name()
         myarch = get_cpu_architecture()
         if platform_name.startswith('x86_64'):
@@ -120,6 +120,8 @@ class EB_tbb(IntelBase, ConfigureMake):
 
     def install_step(self):
         """Custom install step, to add extra symlinks"""
+        install_tbb_lib_path = os.path.join(self.installdir, 'tbb', 'lib')
+
         if self.toolchain.is_system_toolchain():
             silent_cfg_names_map = None
             silent_cfg_extras = None
@@ -146,60 +148,56 @@ class EB_tbb(IntelBase, ConfigureMake):
 
             # determine libdir
             os.chdir(self.installdir)
-            self.libpath = os.path.join('tbb', 'libs', 'intel64')
+            libpath = os.path.join('tbb', 'libs', 'intel64')
             if LooseVersion(self.version) < LooseVersion('4.1.0'):
                 libglob = 'tbb/lib/intel64/cc*libc*_kernel*'
                 libs = sorted(glob.glob(libglob), key=LooseVersion)
-                if len(libs):
+                if libs:
                     # take the last one, should be ordered by cc version
                     # we're only interested in the last bit
-                    libdir = libs[-1].split('/')[-1]
+                    libdir = os.path.basename(libs[-1])
                 else:
                     raise EasyBuildError("No libs found using %s in %s", libglob, self.installdir)
             else:
-                libdir = get_tbb_gccprefix(self.libpath)
+                libdir = get_tbb_gccprefix(libpath)
 
-            self.libpath = os.path.join(self.libpath, libdir)
-            self.log.debug("self.libpath: %s" % self.libpath)
+            libpath = os.path.join(libpath, libdir)
+            self.log.debug("libpath: %s" % libpath)
             # applications go looking into tbb/lib so we move what's in there to libs
-            install_libpath = os.path.join(self.installdir, 'tbb', 'lib')
-            shutil.move(install_libpath, os.path.join(self.installdir, 'tbb', 'libs'))
-            # and symlink the right lib from /tbb/libs/intel64/... to lib
-            os.symlink(os.path.join(self.installdir, self.libpath), install_libpath)
+            shutil.move(install_tbb_lib_path, os.path.join(self.installdir, 'tbb', 'libs'))
+            # and symlink the right folder from /tbb/libs/intel64/... to lib and tbb/lib
+            symlink(libpath, os.path.join(self.installdir, 'lib'), use_abspath_source=False)
+            symlink(os.path.join(self.installdir, libpath), install_tbb_lib_path)
         else:
             # no custom install step when building from source (building is done in install directory)
             cand_lib_paths = glob.glob(os.path.join(self.installdir, 'build', '*_release'))
             if len(cand_lib_paths) == 1:
-                # applications go looking into tbb/lib so we symlink the location where they are built  to lib
-                self.libpath = os.path.join('build', os.path.basename(cand_lib_paths[0]))
-                install_libpath = os.path.join(self.installdir, 'lib')
-                os.symlink(os.path.join(self.installdir, self.libpath), install_libpath)
+                # applications go looking into tbb/lib or lib so we symlink the location where they are built to those 2
+                libpath = os.path.join('build', os.path.basename(cand_lib_paths[0]))
+                symlink(libpath, os.path.join(self.installdir, 'lib'), use_abspath_source=False)
+                symlink(os.path.join(self.installdir, libpath), install_tbb_lib_path)
             else:
                 raise EasyBuildError("Failed to isolate location of libraries: %s", cand_lib_paths)
 
     def sanity_check_step(self):
         """Custom sanity check for TBB"""
         custom_paths = {
-            'files': [],
-            'dirs': [],
+            'files': [
+                os.path.join('lib', 'libtbb.so'),
+                os.path.join('lib', 'libtbbmalloc.so'),
+                os.path.join('tbb', 'lib', 'libtbb.so'),
+                os.path.join('tbb', 'lib', 'libtbbmalloc.so'),
+            ],
+            'dirs': [os.path.join('include', 'tbb')],
         }
         if self.toolchain.is_system_toolchain():
-            custom_paths['dirs'].extend(['tbb/bin', 'tbb/lib', 'tbb/libs'])
-        else:
-            custom_paths['files'].extend([
-                os.path.join(self.libpath, 'libtbb.so'),
-                os.path.join(self.libpath, 'libtbbmalloc.so'),
-            ])
-            custom_paths['dirs'].append('include/tbb')
+            custom_paths['dirs'].extend(os.path.join('tbb', p) for p in ('bin', 'lib', 'libs'))
 
         super(EB_tbb, self).sanity_check_step(custom_paths=custom_paths)
 
     def make_module_extra(self):
         """Add correct path to lib to LD_LIBRARY_PATH. and intel license file"""
         txt = super(EB_tbb, self).make_module_extra()
-
-        txt += self.module_generator.prepend_paths('LD_LIBRARY_PATH', [self.libpath])
-        txt += self.module_generator.prepend_paths('LIBRARY_PATH', [self.libpath])
 
         tbb_subdir = ''
         if os.path.exists(os.path.join(self.installdir, 'tbb')):
