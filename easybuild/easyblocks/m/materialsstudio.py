d0e727a13d8238317d30b073c7efb5379372e7d2
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-07-27 20:22:40 +0000

    supporting materialsstudios 2020

--- easybuild/easyblocks/m/materialsstudio.py
+++ easybuild/easyblocks/m/materialsstudio.py
@@ -29,6 +29,8 @@ import glob
 import os
 import re
 
+from distutils.version import LooseVersion
+
 from easybuild.easyblocks.generic.packedbinary import PackedBinary
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
@@ -42,20 +44,28 @@ class EB_MaterialsStudio(PackedBinary):
         """Build by running the command with the inputfiles"""
         license_server = os.getenv("MS_LICENSE_SERVER")
         home = os.getenv("HOME")
-        cmd = "cd %s/MaterialsStudio* && %s ./install" % (self.builddir, self.cfg['preinstallopts'])
-        qanda = {
-            "The location where Materials Studio will be installed is <install location>/MaterialsStudio18.1 [%s/BIOVIA]" % home: self.installdir,
-            "Please enter the location of a License Pack installation, or an empty directory into which the License Pack will be installed. [%s]" % self.installdir: self.installdir,
-            "%s does not appear to contain a supported License Pack installation. Would you like to install it to that location? [Y/n] [Y]" % self.installdir: 'Y',
-            "Would you like to specify an alternative Gateway port number (default is 18888) [N]": 'N',
-            "Do you wish to start the Gateway service after installation? Answer no here if you wish to configure security settings before starting. (Y/n) [Y]": 'N',
-            "Do you want to change the installation directory? (Y/N/Quit)": 'Y',
-            "To select/deselect type its number: [1] Yes [2] No [3] Cancel >>>[1]": '1',
-            "[ ] 0. Continue >>>": '0',
-            " 1) Enter temporary license password 2) Set connection to license server 3) List command line license administration tools 99) Finished with license configuration Choose one of the above options:": '99'
-        }
-        no_qa = [ "Running installation ...", "Running ConfigureMaterialsStudio.pl" ]
-        run_cmd_qa(cmd, qanda, no_qa = no_qa, log_all=True)
+        if LooseVersion(self.version) < LooseVersion("2020"):
+            cmd = "cd %s/MaterialsStudio* && %s ./install" % (self.builddir, self.cfg['preinstallopts'])
+            qanda = {
+                "The location where Materials Studio will be installed is <install location>/MaterialsStudio18.1 [%s/BIOVIA]" % home: self.installdir,
+                "Please enter the location of a License Pack installation, or an empty directory into which the License Pack will be installed. [%s]" % self.installdir: self.installdir,
+                "%s does not appear to contain a supported License Pack installation. Would you like to install it to that location? [Y/n] [Y]" % self.installdir: 'Y',
+                "Would you like to specify an alternative Gateway port number (default is 18888) [N]": 'N',
+                "Do you wish to start the Gateway service after installation? Answer no here if you wish to configure security settings before starting. (Y/n) [Y]": 'N',
+                "Do you want to change the installation directory? (Y/N/Quit)": 'Y',
+                "To select/deselect type its number: [1] Yes [2] No [3] Cancel >>>[1]": '1',
+                "[ ] 0. Continue >>>": '0',
+                " 1) Enter temporary license password 2) Set connection to license server 3) List command line license administration tools 99) Finished with license configuration Choose one of the above options:": '99'
+            }
+            no_qa = [ "Running installation ...", "Running ConfigureMaterialsStudio.pl" ]
+            run_cmd_qa(cmd, qanda, no_qa = no_qa, log_all=True)
+        else:
+            cmd = 'cd %s/MaterialsStudio* && ./LicensePack/lp_setup_linux.sh --nox11 --target %s/tmp -- -silent -P "licensepack.installLocation=%s"' % (self.builddir, self.builddir, self.installdir)
+            (out, _) = run_cmd(cmd, log_all=True, simple=False)
+
+            cmd = "cd %s/MaterialsStudio* && %s ./install --batch --nonroot --nostart --installroot=%s --lproot=%s" % (self.builddir, self.cfg['preinstallopts'], self.installdir, self.installdir)
+            (out, _) = run_cmd(cmd, log_all=True, simple=False)
+
         cmd = "echo %s >> %s/BIOVIA_LicensePack/Licenses/msi_server.fil" % (license_server, self.installdir)
         run_cmd(cmd, log_all=True)
 
