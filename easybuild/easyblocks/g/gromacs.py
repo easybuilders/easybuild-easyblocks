5e4c9efd6f7d29f6e35f994cfc0c40ac9b39f83d
jhmeinke <68056523+jhmeinke@users.noreply.github.com>
2022-01-20 08:33:20 +0100

    Break line to keep it within 120 characters.

--- easybuild/easyblocks/g/gromacs.py
+++ easybuild/easyblocks/g/gromacs.py
@@ -194,7 +194,8 @@ class EB_GROMACS(CMakeMake):
                 # Set CUDA capabilities based on template value.
                 if '-DGMX_CUDA_TARGET_SM' not in self.cfg['configopts']:
                     cuda_cc_semicolon_sep = self.cfg.get_cuda_cc_template_value("cuda_cc_semicolon_sep").replace('.', '')
-                    self.cfg.update('configopts', '-DGMX_CUDA_TARGET_SM="%s"' % cuda_cc_semicolon_sep)
+                    self.cfg.update('configopts',
+                                    '-DGMX_CUDA_TARGET_SM="%s"' % cuda_cc_semicolon_sep)
             else:
                 # explicitly disable GPU support if CUDA is not available,
                 # to avoid that GROMACS finds and uses a system-wide CUDA compiler
