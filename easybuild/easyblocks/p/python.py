abad96c035e956147377308317e34b5429d9011f
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-02-21 18:14:57 +0100

    avoid dependency on 'which' command in sanity check for Python, use 'command -v' instead

--- easybuild/easyblocks/p/python.py
+++ easybuild/easyblocks/p/python.py
@@ -510,7 +510,7 @@ class EB_Python(ConfigureMake):
 
         abiflags = ''
         if LooseVersion(self.version) >= LooseVersion("3"):
-            run_cmd("which python", log_all=True, simple=False, trace=False)
+            run_cmd("command -v python", log_all=True, simple=False, trace=False)
             cmd = 'python -c "import sysconfig; print(sysconfig.get_config_var(\'abiflags\'));"'
             (abiflags, _) = run_cmd(cmd, log_all=True, simple=False, trace=False)
             if not abiflags:
