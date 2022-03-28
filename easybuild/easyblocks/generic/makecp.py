5c1748a8213a47c3abc3ff4176d3e9eb2b8547a7
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-03-28 17:52:54 +0200

    update copyright lines for 2022

--- easybuild/easyblocks/generic/makecp.py
+++ easybuild/easyblocks/generic/makecp.py
@@ -1,5 +1,5 @@
 ##
-# Copyright 2013 the Cyprus Institute
+# Copyright 2013-2022 the Cyprus Institute
 #
 # This file is part of EasyBuild,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
