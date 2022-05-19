1526e2743d447ec25ac0eedc4f3026ece8960f38
sassy <sassy@hp>
2022-05-19 15:10:26 +0100

    Further cleanup, following the suggestions and help of @ocaisa

--- easybuild/easyblocks/s/superlu.py
+++ easybuild/easyblocks/s/superlu.py
@@ -47,7 +47,7 @@ class EB_SuperLU(CMakeMake):
     @staticmethod
     def extra_options():
         extra_vars = CMakeMake.extra_options()
-        extra_vars['build_shared_libs'][0] = False 
+        extra_vars['build_shared_libs'][0] = True 
         extra_vars['separate_build_dir'][0] = True
         return extra_vars
 
@@ -64,9 +64,13 @@ class EB_SuperLU(CMakeMake):
         Set the CMake options for SuperLU
         """
         # Make sure not to build the slow BLAS library included in the package
-        self.cfg.update('configopts', '-Denable_blaslib=OFF')
         # At least for version 5.3.0 the name has changed:
-        self.cfg.update('configopts', '-Denable_internal_blaslib=OFF')
+        superlu_version = self.version
+        if LooseVersion(superlu_version) >= LooseVersion('5.3'):
+            self.cfg.update('configopts', '-Denable_internal_blaslib=OFF')
+
+        else:
+            self.cfg.update('configopts', '-Denable_blaslib=OFF')
 
         # Set the BLAS library to use
         # For this, use the BLA_VENDOR option from the FindBLAS module of CMake
@@ -77,6 +81,7 @@ class EB_SuperLU(CMakeMake):
             raise EasyBuildError("No BLAS library found in the toolchain")
 
         toolchain_blas = toolchain_blas_list[0]
+        cmake_version = get_software_version('cmake')
         if toolchain_blas == 'imkl':
             imkl_version = get_software_version('imkl')
             if LooseVersion(imkl_version) >= LooseVersion('10'):
@@ -89,9 +94,26 @@ class EB_SuperLU(CMakeMake):
                 # 'Intel' -> For older versions of mkl 32 and 64 bit
                 self.cfg.update('configopts', '-DBLA_VENDOR="Intel"')
 
-        elif toolchain_blas in ['ACML', 'ATLAS', 'OpenBLAS', 'FlexiBLAS']:
+        elif toolchain_blas in ['ACML', 'ATLAS']:
             self.cfg.update('configopts', '-DBLA_VENDOR="%s"' % toolchain_blas)
 
+            if LooseVersion(cmake_version) >= LooseVersion('3.6'):
+                self.cfg.update('configopts', '-DBLA_VENDOR="%s"' % toolchain_blas)
+            else:
+                # Unfortunately, OpenBLAS is not recognized by FindBLAS from CMake,
+                # we have to specify the OpenBLAS library manually
+                openblas_lib = os.path.join(get_software_root('OpenBLAS'), get_software_libdir('OpenBLAS'), "libopenblas.a")
+                self.cfg.update('configopts', '-DBLAS_LIBRARIES="%s;pthread"' % openblas_lib)
+
+        elif toolchain_blas == 'FlexiBLAS':
+            if LooseVersion(cmake_version) >= LooseVersion('3.19'):
+                self.cfg.update('configopts', '-DBLA_VENDOR="%s"' % toolchain_blas)
+            else:
+                # Unfortunately, FlexiBLAS is not recognized by FindBLAS from CMake,
+                # we have to specify the FlexiBLAS library manually
+                flexiblas_lib = os.path.join(get_software_root('FlexiBLAS'), get_software_libdir('FlexiBLAS'),
+                "libflexiblas.so")
+                self.cfg.update('configopts', '-DBLAS_LIBRARIES="%s;pthread"' % flexiblas_lib)
         else:
             # This BLAS library is not supported yet
             raise EasyBuildError("BLAS library '%s' is not supported yet", toolchain_blas)
