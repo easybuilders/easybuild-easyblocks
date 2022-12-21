ac75b46d168621f2c8d7a7521bf0a356bb205955
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-12-21 09:35:19 +0100

    update scipy easyblock for scipy >= 1.9 to correctly configure BLAS/LAPACK library to use via Meson (WIP)

--- easybuild/easyblocks/s/scipy.py
+++ easybuild/easyblocks/s/scipy.py
@@ -32,9 +32,12 @@ EasyBuild support for building and installing scipy, implemented as an easyblock
 @author: Jens Timmerman (Ghent University)
 """
 from distutils.version import LooseVersion
+import os
 
 from easybuild.easyblocks.generic.fortranpythonpackage import FortranPythonPackage
 from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
+from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.filetools import remove_dir
 import easybuild.tools.toolchain as toolchain
 
 
@@ -57,6 +60,48 @@ class EB_scipy(FortranPythonPackage):
             # which requires unsetting $LDFLAGS
             if self.toolchain.comp_family() in [toolchain.GCC, toolchain.CLANGGCC]:  # @UndefinedVariable
                 self.cfg.update('preinstallopts', "unset LDFLAGS && ")
+
+        # configure BLAS/LAPACK library to use with Meson for scipy >= 1.9.0
+        if LooseVersion(self.version) >= LooseVersion('1.9.0'):
+            lapack_lib = self.toolchain.lapack_family()
+            if lapack_lib == toolchain.FLEXIBLAS:
+                blas_lapack = 'flexiblas'
+            elif lapack_lib == toolchain.INTELMKL:
+                blas_lapack = 'mkl'
+            elif lapack_lib == toolchain.OPENBLAS:
+                blas_lapack = 'openblas'
+            else:
+                raise EasyBuildError("Unknown BLAS/LAPACK library used: %s", lapack_lib)
+
+            meson_cmd = ' '.join([
+                'meson',
+                'setup',
+                'build',
+                '-Dblas=' + blas_lapack,
+                '-Dlapack=' + blas_lapack,
+            ])
+            self.cfg.update('preinstallopts', meson_cmd + ' && ')
+
+    def build_step(self):
+        """
+        Custom build step for scipy: don't run "python setup.py build" for scipy >= 1.9.0
+        """
+        if LooseVersion(self.version) < LooseVersion('1.9.0'):
+            super(EB_scipy, self).build_step()
+
+    def install_step(self):
+        """
+        Custom install step for scipy: clean up existing 'build' directory for scipy >= 1.9.0
+        """
+        if LooseVersion(self.version) >= LooseVersion('1.9.0'):
+            # we need to clean up the build directory if it exists already,
+            # to avoid that Meson detects that the build has been configured already
+            # (when installing scipy for running the tests)
+            build_dir = 'build'
+            if os.path.exists(build_dir):
+                remove_dir(build_dir)
+
+        super(EB_scipy, self).install_step()
 
     def sanity_check_step(self, *args, **kwargs):
         """Custom sanity check for scipy."""
