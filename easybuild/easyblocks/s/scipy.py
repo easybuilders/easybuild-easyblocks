182704176f49880be9f0e686dd4636e9e652fb82
Jasper Grimm <jasper.grimm@york.ac.uk>
2023-01-17 15:48:16 +0000

    update scipy easyblock for scipy >= 1.9.0 to use meson/ninja

--- easybuild/easyblocks/s/scipy.py
+++ easybuild/easyblocks/s/scipy.py
@@ -30,33 +30,96 @@ EasyBuild support for building and installing scipy, implemented as an easyblock
 @author: Kenneth Hoste (Ghent University)
 @author: Pieter De Baets (Ghent University)
 @author: Jens Timmerman (Ghent University)
+@author: Jasper Grimm (University of York)
 """
+import os
 from distutils.version import LooseVersion
 
-from easybuild.easyblocks.generic.fortranpythonpackage import FortranPythonPackage
-from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
+import easybuild.tools.environment as env
 import easybuild.tools.toolchain as toolchain
+from easybuild.easyblocks.generic.fortranpythonpackage import FortranPythonPackage
+from easybuild.easyblocks.generic.mesonninja import MesonNinja
+from easybuild.easyblocks.generic.pythonpackage import det_pylibdir
+from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.modules import get_software_root
+from easybuild.tools.run import run_cmd
 
 
-class EB_scipy(FortranPythonPackage):
+class EB_scipy(FortranPythonPackage, MesonNinja):
     """Support for installing the scipy Python package as part of a Python installation."""
 
     def __init__(self, *args, **kwargs):
         """Set scipy-specific test command."""
         super(EB_scipy, self).__init__(*args, **kwargs)
 
+        self.use_meson = LooseVersion(self.version) >= LooseVersion('1.9')
+        self.pylibdir = None
         self.testinstall = True
         self.testcmd = "cd .. && %(python)s -c 'import numpy; import scipy; scipy.test(verbose=2)'"
 
     def configure_step(self):
         """Custom configure step for scipy: set extra installation options when needed."""
-        super(EB_scipy, self).configure_step()
+        self.pylibdir = det_pylibdir()
+        
+        # scipy >= 1.9.0 uses Meson/Ninja
+        if self.use_meson:
+            # configure BLAS/LAPACK library to use with Meson for scipy >= 1.9.0
+            lapack_lib = self.toolchain.lapack_family()
+            if lapack_lib == toolchain.FLEXIBLAS:
+                blas_lapack = 'flexiblas'
+            elif lapack_lib == toolchain.INTELMKL:
+                blas_lapack = 'mkl'
+            elif lapack_lib == toolchain.OPENBLAS:
+                blas_lapack = 'openblas'
+            else:
+                raise EasyBuildError("Unknown BLAS/LAPACK library used: %s", lapack_lib) 
+            
+            configopts = '-Dblas=%(blas_lapack)s -Dlapack=%(blas_lapack)s' % {'blas_lapack': blas_lapack}
+            self.cfg.update('configopts', configopts)
+            
+            pythonpath = os.getenv('PYTHONPATH')
+            env.setvar('PYTHONPATH', os.pathsep.join([os.path.join(self.installdir, self.pylibdir), pythonpath]))
+            
+            path = os.getenv('PATH')
+            env.setvar('PATH', os.pathsep.join([os.path.join(self.installdir, 'bin'), path]))
 
-        if LooseVersion(self.version) >= LooseVersion('0.13'):
-            # in recent scipy versions, additional compilation is done in the install step,
-            # which requires unsetting $LDFLAGS
-            if self.toolchain.comp_family() in [toolchain.GCC, toolchain.CLANGGCC]:  # @UndefinedVariable
-                self.cfg.update('preinstallopts', "unset LDFLAGS && ")
+            MesonNinja.configure_step(self)
+        else:
+            super(EB_scipy, self).configure_step()
+
+            if LooseVersion(self.version) >= LooseVersion('0.13'):
+                # in recent scipy versions, additional compilation is done in the install step,
+                # which requires unsetting $LDFLAGS
+                if self.toolchain.comp_family() in [toolchain.GCC, toolchain.CLANGGCC]:  # @UndefinedVariable
+                    self.cfg.update('preinstallopts', "unset LDFLAGS && ")
+
+    def build_step(self):
+        """Custom build step for scipy: use ninja for scipy >= 1.9.0"""
+        if self.use_meson:
+            MesonNinja.build_step(self)
+        else:
+            super(EB_scipy, self).build_step()
+
+    def test_step(self):
+        """Custom test step for scipy: skip if scipy >= 1.9.0"""
+        if self.use_meson:
+            # need to run `ninja install` before we can run the test
+            pass
+        else:
+            super(EB_scipy, self).build_step()
+
+    def install_step(self):
+        """Custom install step for scipy: use ninja for scipy >= 1.9.0"""
+        if self.use_meson:
+            self.cfg['installopts'] = ""
+            MesonNinja.install_step(self)
+           
+           # also run the test
+            python_root = get_software_root('Python')
+            python_bin = os.path.join(python_root, 'bin', 'python')
+            run_cmd(self.testcmd % {'python': python_bin}, log_all=True, simple=True)
+        else:
+            super(EB_scipy, self).install_step()
 
     def sanity_check_step(self, *args, **kwargs):
         """Custom sanity check for scipy."""
