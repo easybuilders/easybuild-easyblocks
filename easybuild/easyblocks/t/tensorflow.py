9a74bebcde6ca59cbedf164cd1b3b328374810c8
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-08-31 08:36:26 +0200

    fix typo in comment regarding boringssl in TensorFlow easyblock

--- easybuild/easyblocks/t/tensorflow.py
+++ easybuild/easyblocks/t/tensorflow.py
@@ -161,7 +161,7 @@ def get_system_libs_for_version(tf_version, as_valid_libs=False):
     # Software recognized by TF but which is always disabled (usually because no EC is known)
     # Format: <TF name>: <version range>
     unused_system_libs = {
-        'boringssl': '2.0.0:',  # Implied by cURL and existance of OpenSSL anywhere in the dependency chain
+        'boringssl': '2.0.0:',  # Implied by cURL and existence of OpenSSL anywhere in the dependency chain
         'com_github_googleapis_googleapis': '2.0.0:2.5.0',
         'com_github_googlecloudplatform_google_cloud_cpp': '2.0.0:',  # Not used due to $TF_NEED_GCP=0
         'com_github_grpc_grpc': '2.2.0:',
