4273e31e8b232515d02093ce51c06af32a1894b6
Bart Oldeman <bart.oldeman@calculquebec.ca>
2023-11-24 12:37:21 +0000

    import get_software_version for aliases

--- easybuild/easyblocks/i/impi.py
+++ easybuild/easyblocks/i/impi.py
@@ -42,7 +42,7 @@ from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
 from easybuild.tools.config import build_option
 from easybuild.tools.filetools import apply_regex_substitutions, change_dir, extract_file, mkdir, write_file
-from easybuild.tools.modules import get_software_root
+from easybuild.tools.modules import get_software_root, get_software_version
 from easybuild.tools.run import run_cmd
 from easybuild.tools.systemtools import get_shared_lib_ext
 from easybuild.tools.toolchain.mpi import get_mpi_cmd_template
