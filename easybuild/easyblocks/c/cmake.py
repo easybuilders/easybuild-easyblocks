8a3c64d3d6cba671e4cc219645b1e1aa35dae815
MartinsNadia <nadia.martins@hpcnow.com>
2024-01-09 17:21:52 +0000

    Implementation of cmake policy error for python

--- easybuild/easyblocks/c/cmake.py
+++ easybuild/easyblocks/c/cmake.py
@@ -100,8 +100,11 @@ class EB_CMake(ConfigureMake):
         if self.cfg['use_openssl']:
             add_cmake_opts['CMAKE_USE_OPENSSL'] = 'ON'
 
+        cmake_version = get_software_version('CMake')
+
+        if version.parse(cmake_version) >= version.parse('3.15'):
         # Default policy option for Python_NEWPackages
-        add_cmake_opts['MAKE_POLICY_DEFAULT_CMP0094'] = 'NEW'
+            add_cmake_opts['MAKE_POLICY_DEFAULT_CMP0094'] = 'NEW'
 
         cmake_prefix_path = os.environ.get('CMAKE_PREFIX_PATH', '').split(':')
         cmake_library_path = os.environ.get('CMAKE_LIBRARY_PATH', '').split(':')
