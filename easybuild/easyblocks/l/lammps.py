a7a15f30fadf3a9abe1b2b222bdb2cf499addb36
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2024-01-09 16:44:36 +0100

    fix functions update_kokkos_cpu_mapping() and  get_kokkos_arch()

--- easybuild/easyblocks/l/lammps.py
+++ easybuild/easyblocks/l/lammps.py
@@ -210,7 +210,7 @@ class EB_LAMMPS(CMakeMake):
                 KOKKOS_GPU_ARCH_TABLE[cc] = KOKKOS_GPU_ARCH_TABLE[cc].lower().title()
 
         self.kokkos_cpu_mapping = copy.deepcopy(KOKKOS_CPU_MAPPING)
-        update_kokkos_cpu_mapping()
+        self.update_kokkos_cpu_mapping()
 
     @staticmethod
     def extra_options(**kwargs):
@@ -331,7 +331,7 @@ class EB_LAMMPS(CMakeMake):
             self.cfg.update('configopts', pkg_opt + 'on')
 
         # grab the architecture so we can check if we have Intel hardware (also used for Kokkos below)
-        processor_arch, gpu_arch = get_kokkos_arch(cuda_cc, self.cfg['kokkos_arch'], cuda=self.cuda)
+        processor_arch, gpu_arch = get_kokkos_arch(self.kokkos_cpu_mapping, cuda_cc, self.cfg['kokkos_arch'], cuda=self.cuda)
         # arch names changed between some releases :(
         if LooseVersion(self.cur_version) < LooseVersion(self.ref_version):
             if processor_arch in KOKKOS_LEGACY_ARCH_MAPPING.keys():
@@ -523,7 +523,7 @@ def get_cuda_gpu_arch(cuda_cc):
     return 'sm_%s' % str(sorted(cuda_cc, reverse=True)[0]).replace(".", "")
 
 
-def get_kokkos_arch(cuda_cc, kokkos_arch, cuda=None):
+def get_kokkos_arch(mapping, cuda_cc, kokkos_arch, cuda=None):
     """
     Return KOKKOS ARCH in LAMMPS required format, which is 'CPU_ARCH' and 'GPU_ARCH'.
 
@@ -546,7 +546,7 @@ def get_kokkos_arch(cuda_cc, kokkos_arch, cuda=None):
         warning_msg = "kokkos_arch not set. Trying to auto-detect CPU arch."
         print_warning(warning_msg)
 
-        processor_arch = self.kokkos_cpu_mapping.get(get_cpu_arch())
+        processor_arch =  mapping.get(get_cpu_arch())
 
         if not processor_arch:
             error_msg = "Couldn't determine CPU architecture, you need to set 'kokkos_arch' manually."
