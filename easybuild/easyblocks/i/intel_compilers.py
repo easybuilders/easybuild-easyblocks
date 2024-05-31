2b207a22eee8028bcb568b8d05b9ca28546229a0
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2024-05-31 15:34:37 +0200

    fix too long line

--- easybuild/easyblocks/i/intel_compilers.py
+++ easybuild/easyblocks/i/intel_compilers.py
@@ -177,7 +177,8 @@ class EB_intel_minus_compilers(IntelBase):
         multiarch_out, ec = run_cmd("gcc -print-multiarch", simple=False)
         multiarch_out = multiarch_out.strip()
         if ec == 0 and multiarch_out:
-            multiarch_inc_dir, ec = run_cmd("gcc -E -Wp,-v -xc /dev/null 2>&1 | grep %s$ | grep -v GCCcore" % multiarch_out)
+            multi_arch_inc_dir_cmd = "gcc -E -Wp,-v -xc /dev/null 2>&1 | grep %s$ | grep -v GCCcore" % multiarch_out
+            multiarch_inc_dir, ec = run_cmd(multi_arch_inc_dir_cmd)
             if ec == 0 and multiarch_inc_dir:
                 multiarch_inc_dir = multiarch_inc_dir.strip()
                 self.log.info("Adding multiarch include path %s to $CPATH in generated module file", multiarch_inc_dir)
