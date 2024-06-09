9d6cea0d66d1ee339aad2d5d431d7d1ac64453f3
Thang Ha <thanghn90@gmail.com>
2024-06-09 00:29:28 -0500

    move comment one line down
    As suggested by bot "hound"

--- easybuild/easyblocks/w/wrf.py
+++ easybuild/easyblocks/w/wrf.py
@@ -56,7 +56,8 @@ def det_wrf_subdir(wrf_version):
 
     if LooseVersion(wrf_version) < LooseVersion('4.0'):
         wrf_subdir = 'WRFV%s' % wrf_version.split('.')[0]
-    elif LooseVersion(wrf_version) == LooseVersion('4.5.1'): # WRF 4.5.2 actually use "WRF-4.5.2" subdirectory naming scheme
+    elif LooseVersion(wrf_version) == LooseVersion('4.5.1'):
+        # WRF 4.5.2 actually use "WRF-4.5.2" subdirectory naming scheme
         wrf_subdir = 'WRFV%s' % wrf_version
     else:
         wrf_subdir = 'WRF-%s' % wrf_version
