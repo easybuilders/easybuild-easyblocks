e8d88a47d734d6764ccc9328617bb4cc4ccedf2b
e0958350 <e0958350@atlas8-c01>
2024-07-08 11:31:22 +0800

    fix format error

--- easybuild/easyblocks/b/blis.py
+++ easybuild/easyblocks/b/blis.py
@@ -3,6 +3,7 @@ from easybuild.tools.systemtools import get_shared_lib_ext
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.systemtools import get_cpu_arch_name
 
+
 class EB_BLIS(ConfigureMake):
     """Custom easyblock for BLIS"""
 
@@ -16,7 +17,6 @@ class EB_BLIS(ConfigureMake):
 
         return ConfigureMake.extra_options(extra_vars)
 
-
     def configure_step(self):
         """Custom configure step for BLIS."""
         if self.cfg['cblas_enable']:
@@ -24,11 +24,11 @@ class EB_BLIS(ConfigureMake):
 
         if self.cfg['shared_enable']:
             self.cfg.update('configopts', '--enable-shared')
-      
+
         arch_name = get_cpu_arch_name()
         if arch_name == "a64fx":
-            self.cfg.update('configopts', 'CFLAGS="$CFLAGS -DCACHE_SECTOR_SIZE_READONLY"')        
- 
+            self.cfg.update('configopts', 'CFLAGS="$CFLAGS -DCACHE_SECTOR_SIZE_READONLY"')
+
         self.cfg.update('configopts', '--enable-threading=%s CC="$CC" auto' % self.cfg['multi_threading_type'])
 
         super(EB_BLIS, self).configure_step()
