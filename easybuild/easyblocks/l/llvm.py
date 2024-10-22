e2dab7aff7734436355b3373f08b0a083838ba1a
crivella <davide.grassano@epfl.ch>
2024-10-22 17:45:07 +0200

    Ensure `zlib` and `zstd` are not used if not provided as deps

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -437,8 +437,10 @@ class EB_LLVM(CMakeMake):
         """General configuration step for LLVM."""
         self._cmakeopts['LLVM_ENABLE_ASSERTIONS'] = 'ON' if self.cfg['assertions'] else 'OFF'
 
-        if get_software_root('zlib'):
-            self._cmakeopts['LLVM_ENABLE_ZLIB'] = 'ON'
+        # If on risk finding a system zlib or zstd leading to including /usr/include as -isystem that can lead
+        # to errors during compilation of `offload.tools.kernelreplay`` due to the inclusion of LLVMSupport (19.x)
+        self._cmakeopts['LLVM_ENABLE_ZLIB'] = 'ON' if get_software_root('zlib') else 'OFF'
+        self._cmakeopts['LLVM_ENABLE_ZSTD'] = 'ON' if get_software_root('zstd') else 'OFF'
 
         z3_root = get_software_root("Z3")
         if z3_root:
@@ -737,22 +739,22 @@ class EB_LLVM(CMakeMake):
         else:
             self.log.info("Building LLVM")
             print_msg("Building stage 1/1")
-        change_dir(self.llvm_obj_dir_stage1)
-        super(EB_LLVM, self).build_step(verbose, path)
-        # import shutil
-        # change_dir(self.builddir)
-        # print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
-        # shutil.rmtree('llvm.obj.1', ignore_errors=True)
-        # shutil.copytree(os.path.join('..', 'llvm.obj.1'), 'llvm.obj.1')
+        # change_dir(self.llvm_obj_dir_stage1)
+        # super(EB_LLVM, self).build_step(verbose, path)
+        import shutil
+        change_dir(self.builddir)
+        print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
+        shutil.rmtree('llvm.obj.1', ignore_errors=True)
+        shutil.copytree(os.path.join('..', 'llvm.obj.1'), 'llvm.obj.1')
         if self.cfg['bootstrap']:
             self.log.info("Building stage 2")
             print_msg("Building stage 2/3")
-            self.configure_step2()
-            self.build_with_prev_stage(self.llvm_obj_dir_stage1, self.llvm_obj_dir_stage2)
-            # change_dir(self.builddir)
-            # print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
-            # shutil.rmtree('llvm.obj.2', ignore_errors=True)
-            # shutil.copytree(os.path.join('..', 'llvm.obj.2'), 'llvm.obj.2')
+            # self.configure_step2()
+            # self.build_with_prev_stage(self.llvm_obj_dir_stage1, self.llvm_obj_dir_stage2)
+            change_dir(self.builddir)
+            print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
+            shutil.rmtree('llvm.obj.2', ignore_errors=True)
+            shutil.copytree(os.path.join('..', 'llvm.obj.2'), 'llvm.obj.2')
 
             self.log.info("Building stage 3")
             print_msg("Building stage 3/3")
