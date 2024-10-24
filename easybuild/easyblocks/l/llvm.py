6cf5064aaab1f5fad081a1f239ffb5a205b3e38c
crivella <davide.grassano@epfl.ch>
2024-10-24 13:15:44 +0200

    Revert

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -82,6 +82,7 @@ remove_gcc_dependency_opts = {
     'LIBCXX_USE_COMPILER_RT': 'On',
     'LIBCXX_CXX_ABI': 'libcxxabi',
     'LIBCXX_DEFAULT_ABI_LIBRARY': 'libcxxabi',
+    'LIBCXX_HAS_ATOMIC_LIB': 'NO',  # Needed as libatomic could not be present on the system
 
     'LIBCXXABI_USE_LLVM_UNWINDER': 'On',
     'LIBCXXABI_USE_COMPILER_RT': 'On',
@@ -739,22 +740,22 @@ class EB_LLVM(CMakeMake):
         else:
             self.log.info("Building LLVM")
             print_msg("Building stage 1/1")
-        # change_dir(self.llvm_obj_dir_stage1)
-        # super(EB_LLVM, self).build_step(verbose, path)
-        import shutil
-        change_dir(self.builddir)
-        print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
-        shutil.rmtree('llvm.obj.1', ignore_errors=True)
-        shutil.copytree(os.path.join('..', 'llvm.obj.1'), 'llvm.obj.1')
+        change_dir(self.llvm_obj_dir_stage1)
+        super(EB_LLVM, self).build_step(verbose, path)
+        # import shutil
+        # change_dir(self.builddir)
+        # print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
+        # shutil.rmtree('llvm.obj.1', ignore_errors=True)
+        # shutil.copytree(os.path.join('..', 'llvm.obj.1'), 'llvm.obj.1')
         if self.cfg['bootstrap']:
             self.log.info("Building stage 2")
             print_msg("Building stage 2/3")
-            # self.configure_step2()
-            # self.build_with_prev_stage(self.llvm_obj_dir_stage1, self.llvm_obj_dir_stage2)
-            change_dir(self.builddir)
-            print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
-            shutil.rmtree('llvm.obj.2', ignore_errors=True)
-            shutil.copytree(os.path.join('..', 'llvm.obj.2'), 'llvm.obj.2')
+            self.configure_step2()
+            self.build_with_prev_stage(self.llvm_obj_dir_stage1, self.llvm_obj_dir_stage2)
+            # change_dir(self.builddir)
+            # print_msg("TESTING!!!: Copying from previosu build (REMOVE ME)")
+            # shutil.rmtree('llvm.obj.2', ignore_errors=True)
+            # shutil.copytree(os.path.join('..', 'llvm.obj.2'), 'llvm.obj.2')
 
             self.log.info("Building stage 3")
             print_msg("Building stage 3/3")
