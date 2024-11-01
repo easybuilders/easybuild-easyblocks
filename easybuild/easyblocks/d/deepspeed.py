95224352732c0adbb2f5a353d72e23cc8d773f59
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2024-11-01 14:02:45 +0000

    Remove unused imports

--- easybuild/easyblocks/d/deepspeed.py
+++ easybuild/easyblocks/d/deepspeed.py
@@ -27,9 +27,6 @@ EasyBuild support for building and installing DeepSpeed, implemented as an easyb
 
 author: Viktor Rehnberg (Chalmers University of Technology)
 """
-import os
-import re
-
 from easybuild.easyblocks.generic.pythonpackage import PythonPackage
 from easybuild.framework.easyconfig import CUSTOM
 from easybuild.tools.build_log import EasyBuildError
