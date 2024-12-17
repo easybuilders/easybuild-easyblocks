6f8850291285c06e2113f451af5e035cc07b4f38
Alex Domingo <alex.domingo.toro@vub.be>
2024-12-17 23:47:42 +0100

    remove support for versions of icc prior to v2020.x from vtune easyblock

--- easybuild/easyblocks/v/vtune.py
+++ easybuild/easyblocks/v/vtune.py
@@ -37,6 +37,7 @@ from easybuild.easyblocks.generic.intelbase import IntelBase, ACTIVATION_NAME_20
 class EB_VTune(IntelBase):
     """
     Support for installing Intel VTune
+    - minimum version suported: 2020.x
     """
 
     def __init__(self, *args, **kwargs):
@@ -52,36 +53,15 @@ class EB_VTune(IntelBase):
             self.subdir = os.path.join('vtune', self.version)
         elif loosever >= LooseVersion('2020'):
             self.subdir = 'vtune_profiler'
-        elif loosever >= LooseVersion('2018'):
-            self.subdir = 'vtune_amplifier'
-        elif loosever >= LooseVersion('2013_update12'):
-            self.subdir = 'vtune_amplifier_xe'
 
     def prepare_step(self, *args, **kwargs):
         """Since 2019u3 there is no license required."""
-        if LooseVersion(self.version) >= LooseVersion('2019_update3'):
-            kwargs['requires_runtime_license'] = False
+        kwargs['requires_runtime_license'] = False
         super(EB_VTune, self).prepare_step(*args, **kwargs)
 
     def make_installdir(self):
         """Do not create installation directory, install script handles that already."""
         super(EB_VTune, self).make_installdir(dontcreate=True)
-
-    def install_step(self):
-        """
-        Actual installation
-        - create silent cfg file
-        - execute command
-        """
-        silent_cfg_names_map = None
-
-        if LooseVersion(self.version) <= LooseVersion('2013_update11'):
-            silent_cfg_names_map = {
-                'activation_name': ACTIVATION_NAME_2012,
-                'license_file_name': LICENSE_FILE_NAME_2012,
-            }
-
-        super(EB_VTune, self).install_step(silent_cfg_names_map=silent_cfg_names_map)
 
     def make_module_req_guess(self):
         """Find reasonable paths for VTune"""
@@ -89,17 +69,8 @@ class EB_VTune(IntelBase):
 
     def sanity_check_step(self):
         """Custom sanity check paths for VTune."""
-        if LooseVersion(self.version) >= LooseVersion('2020'):
-            binaries = ['amplxe-feedback', 'amplxe-runss', 'vtune', 'vtune-gui']
-        else:
-            binaries = ['amplxe-cl', 'amplxe-feedback', 'amplxe-gui', 'amplxe-runss']
-
+        binaries = ['amplxe-feedback', 'amplxe-runss', 'vtune', 'vtune-gui']
         custom_paths = self.get_custom_paths_tools(binaries)
-
-        custom_commands = []
-        if LooseVersion(self.version) >= LooseVersion('2020'):
-            custom_commands.append('vtune --version')
-        else:
-            custom_commands.append('amplxe-cl --version')
+        custom_commands = ['vtune --version']
 
         super(EB_VTune, self).sanity_check_step(custom_paths=custom_paths, custom_commands=custom_commands)
