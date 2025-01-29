a7267f38309652e337d5ea9e71daa1c5cb9c8b13
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2025-01-29 23:56:28 +0100

    Make sure libraries in /lib/server are also found

--- easybuild/easyblocks/j/java.py
+++ easybuild/easyblocks/j/java.py
@@ -110,6 +110,8 @@ class EB_Java(PackedBinary):
             if library_path:
                 self.log.info("List of library paths from LIBRARY_PATH to be added to RPATH section: %s", library_path)
                 extra_rpaths += library_path
+            # Some libraries, e.g. libjvm.so, are located in $EBROOTJAVA/lib/server, so let's add $ORIGIN/../lib/server
+            extra_paths += ['$ORIGIN/../lib/server']
 
         # If using sysroot AND rpath, add list of library paths in sysroot to consider for adding to RPATH section
         if sysroot and self.toolchain.use_rpath:
