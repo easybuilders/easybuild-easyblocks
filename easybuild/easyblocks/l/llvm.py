3060f80fb30e7ac789a9b6aaa23ab460832a8b78
Jan André Reuter <j.reuter@fz-juelich.de>
2025-03-30 18:07:53 +0200

    LLVM: Switch to report_test_failure during test_step
    Switch to `report_test_failure` so that the build option
    `ignore_test_failure` is correctly handled. Previously,
    the raised exception would cause EasyBuild to abort,
    without a way to work around this easily.
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -935,10 +935,10 @@ class EB_LLVM(CMakeMake):
             max_failed = self.cfg['test_suite_max_failed']
             num_failed = self._para_test_step(parallel=1)
             if num_failed is None:
-                raise EasyBuildError("Failed to extract test results from output")
+                self.report_test_failure("Failed to extract test results from output")
 
             if num_failed > max_failed:
-                raise EasyBuildError(f"Too many failed tests: {num_failed} ({max_failed} allowed)")
+                self.report_test_failure(f"Too many failed tests: {num_failed} ({max_failed} allowed)")
             elif num_failed:
                 self.log.info(f"Test suite completed with {num_failed} failed tests ({max_failed} allowed)")
             else:
