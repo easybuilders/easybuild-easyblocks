70f9e813b26fa375d789671daa77c10f4e2434eb
Jan André Reuter <j.reuter@fz-juelich.de>
2025-03-31 12:24:24 +0200

    LLVM: Escape LIBOMPTARGET_PLUGINS_TO_BUILD

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -468,7 +468,7 @@ class EB_LLVM(CMakeMake):
 
         if 'openmp' in self.final_projects:
             if LooseVersion(self.version) >= LooseVersion('19') and self.cfg['build_openmp_offload']:
-                self.runtimes_cmake_args['LIBOMPTARGET_PLUGINS_TO_BUILD'] = '"%s"' % ';'.join(self.offload_targets)
+                self.runtimes_cmake_args['LIBOMPTARGET_PLUGINS_TO_BUILD'] = '\\"%s\\"' % ';'.join(self.offload_targets)
             self._cmakeopts['OPENMP_ENABLE_LIBOMPTARGET'] = 'ON'
             self._cmakeopts['LIBOMP_INSTALL_ALIASES'] = 'OFF'
             if not self.cfg['build_openmp_tools']:
