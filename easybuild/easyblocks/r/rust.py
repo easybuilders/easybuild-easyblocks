b5223a98bf746945742a642d7523141645b112a8
Joan Aluja Oraá <joan.aluja_oraa@contractors.roche.com>
2025-04-08 15:57:50 +0200

    Set Rust channel to stable to prevent usage of rust-lld as default linker

--- easybuild/easyblocks/r/rust.py
+++ easybuild/easyblocks/r/rust.py
@@ -100,6 +100,13 @@ class EB_Rust(ConfigureMake):
         # old llvm builds from CI get deleted after a certain time
         self.cfg.update('configopts', "--set=llvm.download-ci-llvm=false")
 
+        # set channel to "stable", otherwise Rust will be built as a nightly
+        # nightly builds default to using rust-lld instead of the default linker
+        # this causes issues on RPATH enabled deployments, as there is no RPATH wrapper
+        # for rust-lld.
+        # more info: https://blog.rust-lang.org/2024/05/17/enabling-rust-lld-on-linux.html
+        self.cfg.update('configopts', "--set=rust.channel=stable")
+
         # don't use Ninja if it is not listed as a build dependency;
         # may be because Ninja requires Python, and Rust is a build dependency for cryptography
         # which may be included as an extension with Python
