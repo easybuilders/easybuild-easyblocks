98ff1200861857ddb9d412a8eaf80400cb1728a9
Jan André Reuter <j.reuter@fz-juelich.de>
2025-04-23 15:32:30 +0200

    LLVM: Use pipe to split runtime arguments
    While not officially documented somewhere, this is used in multiple
    places within LLVM to pass runtime arguments.
    This seems to work for LLVM 19, but has to be verified with other
    versions as well.
    
    Signed-off-by: Jan André Reuter <j.reuter@fz-juelich.de>

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -465,7 +465,7 @@ class EB_LLVM(CMakeMake):
 
         if 'openmp' in self.final_projects:
             if LooseVersion(self.version) >= LooseVersion('19') and self.cfg['build_openmp_offload']:
-                self.runtimes_cmake_args['LIBOMPTARGET_PLUGINS_TO_BUILD'] = '%s' % ';'.join(self.offload_targets)
+                self.runtimes_cmake_args['LIBOMPTARGET_PLUGINS_TO_BUILD'] = '%s' % '|'.join(self.offload_targets)
             self._cmakeopts['OPENMP_ENABLE_LIBOMPTARGET'] = 'ON'
             self._cmakeopts['LIBOMP_INSTALL_ALIASES'] = 'OFF'
             if not self.cfg['build_openmp_tools']:
@@ -638,7 +638,7 @@ class EB_LLVM(CMakeMake):
             gpu_archs += ['sm_%s' % cc for cc in self.cuda_cc]
             gpu_archs += self.amd_gfx
             if gpu_archs:
-                self.runtimes_cmake_args['LIBOMPTARGET_DEVICE_ARCHITECTURES'] = '%s' % ';'.join(gpu_archs)
+                self.runtimes_cmake_args['LIBOMPTARGET_DEVICE_ARCHITECTURES'] = '%s' % '|'.join(gpu_archs)
 
         self._configure_general_build()
         self.add_cmake_opts()
