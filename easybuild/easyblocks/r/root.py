66c37f96c6ee98c3e3b70b373e67091a2779d94d
Simon Branford <s.j.branford@bham.ac.uk>
2025-07-14 19:36:03 +0100

    cleanup ancient version support in `root.py`

--- easybuild/easyblocks/r/root.py
+++ easybuild/easyblocks/r/root.py
@@ -43,18 +43,6 @@ from easybuild.tools.run import run_shell_cmd
 
 class EB_ROOT(CMakeMake):
 
-    @staticmethod
-    def extra_options():
-        """
-        Define extra options needed by Geant4
-        """
-        extra_vars = CMakeMake.extra_options()
-        extra_vars.update({
-            'arch': [None, "Target architecture", CUSTOM],
-        })
-        extra_vars['separate_build_dir'][0] = True
-        return extra_vars
-
     def __init__(self, *args, **kwargs):
         """Easyblock constructor."""
         super().__init__(*args, **kwargs)
@@ -85,58 +73,41 @@ class EB_ROOT(CMakeMake):
                 self.log.info("Filtered %s from $CPATH to avoid using LLVM loaded as indirect dependency", llvm_inc)
                 setvar('CPATH', os.pathsep.join(new_cpath))
 
-        # using ./configure is deprecated/broken in recent versions, need to use CMake instead
-        if LooseVersion(self.version.lstrip('v')) >= LooseVersion('6.10'):
-            if self.cfg['arch']:
-                raise EasyBuildError("Specified value '%s' for 'arch' is not used, should not be set", self.cfg['arch'])
+        cfitsio_root = get_software_root('CFITSIO')
+        if cfitsio_root:
+            self.cfg.update('configopts', '-DCFITSIO=%s' % cfitsio_root)
 
-            cfitsio_root = get_software_root('CFITSIO')
-            if cfitsio_root:
-                self.cfg.update('configopts', '-DCFITSIO=%s' % cfitsio_root)
+        fftw_root = get_software_root('FFTW')
+        if fftw_root:
+            self.cfg.update('configopts', '-Dbuiltin_fftw3=OFF -DFFTW_DIR=%s' % fftw_root)
 
-            fftw_root = get_software_root('FFTW')
-            if fftw_root:
-                self.cfg.update('configopts', '-Dbuiltin_fftw3=OFF -DFFTW_DIR=%s' % fftw_root)
+        gsl_root = get_software_root('GSL')
+        if gsl_root:
+            self.cfg.update('configopts', '-DGSL_DIR=%s' % gsl_root)
 
-            gsl_root = get_software_root('GSL')
-            if gsl_root:
-                self.cfg.update('configopts', '-DGSL_DIR=%s' % gsl_root)
+        mesa_root = get_software_root('Mesa')
+        if mesa_root:
+            self.cfg.update('configopts', '-DDOPENGL_INCLUDE_DIR=%s' % os.path.join(mesa_root, 'include'))
+            self.cfg.update('configopts', '-DOPENGL_gl_LIBRARY=%s' % os.path.join(mesa_root, 'lib', 'libGL.so'))
 
-            mesa_root = get_software_root('Mesa')
-            if mesa_root:
-                self.cfg.update('configopts', '-DDOPENGL_INCLUDE_DIR=%s' % os.path.join(mesa_root, 'include'))
-                self.cfg.update('configopts', '-DOPENGL_gl_LIBRARY=%s' % os.path.join(mesa_root, 'lib', 'libGL.so'))
+        python_root = get_software_root('Python')
+        if python_root:
+            pyshortver = '.'.join(get_software_version('Python').split('.')[:2])
+            self.cfg.update('configopts', '-DPYTHON_EXECUTABLE=%s' % os.path.join(python_root, 'bin', 'python'))
+            python_inc_dir = find_glob_pattern(os.path.join(python_root, 'include', 'python%s*' % pyshortver))
+            self.cfg.update('configopts', '-DPYTHON_INCLUDE_DIR=%s' % python_inc_dir)
+            python_lib = find_glob_pattern(
+                os.path.join(python_root, 'lib', 'libpython%s*.so' % pyshortver))
+            self.cfg.update('configopts', '-DPYTHON_LIBRARY=%s' % python_lib)
 
-            python_root = get_software_root('Python')
-            if python_root:
-                pyshortver = '.'.join(get_software_version('Python').split('.')[:2])
-                self.cfg.update('configopts', '-DPYTHON_EXECUTABLE=%s' % os.path.join(python_root, 'bin', 'python'))
-                python_inc_dir = find_glob_pattern(os.path.join(python_root, 'include', 'python%s*' % pyshortver))
-                self.cfg.update('configopts', '-DPYTHON_INCLUDE_DIR=%s' % python_inc_dir)
-                python_lib = find_glob_pattern(
-                    os.path.join(python_root, 'lib', 'libpython%s*.so' % pyshortver))
-                self.cfg.update('configopts', '-DPYTHON_LIBRARY=%s' % python_lib)
+        if get_software_root('X11'):
+            self.cfg.update('configopts', '-Dx11=ON')
 
-            if get_software_root('X11'):
-                self.cfg.update('configopts', '-Dx11=ON')
+        sysroot = build_option('sysroot')
+        if sysroot:
+            self.cfg.update('configopts', '-DDEFAULT_SYSROOT=%s' % sysroot)
 
-            sysroot = build_option('sysroot')
-            if sysroot:
-                self.cfg.update('configopts', '-DDEFAULT_SYSROOT=%s' % sysroot)
-
-            CMakeMake.configure_step(self)
-        else:
-            if self.cfg['arch'] is None:
-                raise EasyBuildError("No architecture specified to pass to configure script")
-
-            self.cfg.update('configopts', "--etcdir=%s/etc/root " % self.installdir)
-
-            cmd = "%s ./configure %s --prefix=%s %s" % (self.cfg['preconfigopts'],
-                                                        self.cfg['arch'],
-                                                        self.installdir,
-                                                        self.cfg['configopts'])
-
-            run_shell_cmd(cmd)
+        super().configure_step()
 
     def sanity_check_step(self):
         """Custom sanity check for ROOT."""
