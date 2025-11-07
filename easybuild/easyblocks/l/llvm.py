dfe2444d24de952a0bb03a4f2c64ecc72bd4c387
Alexander Grund <alexander.grund@tu-dresden.de>
2025-11-07 15:24:55 +0100

    Add `test_suite_max_parallel` option to LLVM
    Allow to limit number of parallel test tasks.

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -232,6 +232,8 @@ class EB_LLVM(CMakeMake):
             'test_suite_ignore_timeouts': [False, "Do not treat timedoud tests as failures", CUSTOM],
             'test_suite_include_benchmarks': [False, "Include benchmarks in the LLVM tests (default False)", CUSTOM],
             'test_suite_max_failed': [0, "Maximum number of failing tests (does not count allowed failures)", CUSTOM],
+            'test_suite_max_parallel': [None, "Maximum number LIT tasks to use for tests. "
+                                        "Limitted by the global parallel setting.", CUSTOM],
             'test_suite_timeout_single': [None, "Timeout for each individual test in the test suite", CUSTOM],
             'test_suite_timeout_total': [None, "Timeout for total running time of the testsuite", CUSTOM],
             'use_pic': [True, "Build with Position Independent Code (PIC)", CUSTOM],
@@ -644,9 +646,13 @@ class EB_LLVM(CMakeMake):
             self._cmakeopts[cmake_flag] = include_benchmarks
             self.runtimes_cmake_args[cmake_flag] = include_benchmarks
 
-        # Make sure tests are not running with more than 'parallel' tasks
+        # Set parallel LIT tasks. Limit by set max-parallelism and in absence of that
+        # use --mpi-tests to determine if parallelism is desired.
         parallel = self.cfg.parallel
-        if not build_option('mpi_tests'):
+        max_parallel = self.cfg['test_suite_max_parallel']
+        if max_parallel:
+            parallel = min(max_parallel, parallel)
+        elif not build_option('mpi_tests'):
             parallel = 1
         lit_args = [f'-j {parallel}']
         if self.cfg['debug_tests']:
