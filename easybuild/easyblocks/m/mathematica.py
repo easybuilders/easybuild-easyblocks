37655b78907f88c3b0eaa0ea648d61a71dde404a
Mikael Öhman <micketeer@gmail.com>
2025-11-18 18:44:10 +0100

    Update easybuild/easyblocks/m/mathematica.py

--- easybuild/easyblocks/m/mathematica.py
+++ easybuild/easyblocks/m/mathematica.py
@@ -154,6 +154,6 @@ class EB_Mathematica(Binary):
         if LooseVersion(self.version) < LooseVersion("14.1"):
             custom_commands = ['mathematica --version']
         else:
-            custom_commands = ['WolframKernel -run "Exit[]"']
+            custom_commands = ['wolframnb --version']
 
         super(EB_Mathematica, self).sanity_check_step(custom_paths=custom_paths, custom_commands=custom_commands)
