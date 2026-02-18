4b5b449ca9e1382cf22b1c755b0ce39eafc64beb
Maxim <maxim.vadimovich@gmail.com>
2026-02-18 16:55:48 +0100

    Modify 'with_cxx_backtrace' option handling in gcc.py
    Change 'with_cxx_backtrace' default value to None and update condition for enabling libstdc++_libbacktrace.

--- easybuild/easyblocks/g/gcc.py
+++ easybuild/easyblocks/g/gcc.py
@@ -156,7 +156,7 @@ class EB_GCC(ConfigureMake):
             'withppl': [False, "Build GCC with PPL support", CUSTOM],
             'withnvptx': [False, "Build GCC with NVPTX offload support", CUSTOM],
             'withamdgcn': [False, "Build GCC with AMD GCN offload support", CUSTOM],
-            'with_cxx_backtrace': [False, "Build GCC with enabled stack trace for C++23", CUSTOM],
+            'with_cxx_backtrace': [None, "Build GCC with enabled stack trace for C++23", CUSTOM],
         }
         return ConfigureMake.extra_options(extra_vars)
 
@@ -644,9 +644,12 @@ class EB_GCC(ConfigureMake):
         else:
             self.configopts += " --disable-lto"
 
-        # enable builind of libstdc++_libbacktrace
-        if self.cfg['with_cxx_backtrace']:
-            self.configopts += " --enable-libstdcxx-backtrace"
+        # enable building of libstdc++_libbacktrace
+        if self.cfg['with_cxx_backtrace'] is not None:
+            if self.cfg['with_cxx_backtrace']:
+                # '--enable-libstdcxx-backtrace' is undefined by default. No need to explicitly 
+                # disable it if 'with_cxx_backtrace' is set to False
+                self.configopts += " --enable-libstdcxx-backtrace"
 
         # configure for a release build
         self.configopts += " --enable-checking=release "
