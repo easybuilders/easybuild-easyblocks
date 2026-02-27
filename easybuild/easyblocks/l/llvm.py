b0a7eef7d7f231f8504ef3c924907f083f2c6ace
Mikael Öhman <micketeer@gmail.com>
2026-02-27 23:39:24 +0100

    Include Thyres suggestions

--- easybuild/easyblocks/l/llvm.py
+++ easybuild/easyblocks/l/llvm.py
@@ -1613,9 +1613,6 @@ class EB_LLVM(CMakeMake):
 
     def sanity_check_step(self, custom_paths=None, custom_commands=None, *args, **kwargs):
         """Perform sanity checks on the installed LLVM."""
-        lib_dir_runtime = None
-        if self.cfg['build_runtimes']:
-            lib_dir_runtime = self.get_runtime_lib_path(self.installdir)
         shlib_ext = '.' + get_shared_lib_ext()
 
         if not hasattr(self, 'nvptx_target_cond'):
@@ -1807,6 +1804,10 @@ class EB_LLVM(CMakeMake):
         if self.cfg['python_bindings']:
             custom_commands += ["python -c 'import clang'"]
             custom_commands += ["python -c 'import mlir'"]
+
+        lib_dir_runtime = None
+        if self.cfg['build_runtimes'] or check_librt_files:
+            lib_dir_runtime = self.get_runtime_lib_path(self.installdir)
 
         check_files.extend(os.path.join('bin', x) for x in check_bin_files)
         check_files.extend(os.path.join('lib', x) for x in check_lib_files)
