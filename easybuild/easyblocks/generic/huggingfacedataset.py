ca8f01353fa9347dead8e5b60f034a701bedb901
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2026-05-24 10:23:17 +0200

    Use save_to_disk over load_dataset
    load_dataset wants to reproduce the dataset in HF_HOME each time
    
    using save_to_disk and load_from_disk should be more canonical

--- easybuild/easyblocks/generic/huggingfacedataset.py
+++ easybuild/easyblocks/generic/huggingfacedataset.py
@@ -28,11 +28,13 @@ EasyBuild support for installing datasets
 @author: Viktor Rehnberg (Chalmers University of Technology)
 """
 import os
+from tempfile import TemporaryDirectory
 
 from easybuild.easyblocks.generic.dataset import Dataset
 from easybuild.framework.easyconfig import CUSTOM, MANDATORY
-from easybuild.tools.filetools import change_dir, clean_dir, expand_glob_paths, find_glob_pattern, mkdir, move_file
-from easybuild.tools.filetools import write_file
+from easybuild.tools.build_log import EasyBuildError
+from easybuild.tools.filetools import change_dir, clean_dir, dir_contains_files, expand_glob_paths, find_glob_pattern
+from easybuild.tools.filetools import mkdir, move_file, write_file
 from easybuild.tools.run import run_shell_cmd
 
 
@@ -50,6 +52,10 @@ class HuggingFaceDataset(Dataset):
         })
         return extra_vars
 
+    @property
+    def _build_dataset_dir(self):
+        return os.path.join(self.builddir, "saved_dataset")
+
     def __init__(self, *args, **kwargs):
         '''Initialize HuggingFaceDataset-specific variables.'''
         super().__init__(*args, **kwargs)
@@ -58,15 +64,20 @@ class HuggingFaceDataset(Dataset):
     def build_step(self):
         '''Build up cache_dir with dataset'''
 
-        # Prepare download directory of cache_dir
         change_dir(self.builddir)
-        _hf_cache_download_dir = os.path.join(self.builddir, 'downloads')
+        _hf_home_dir = os.path.join(self.builddir, 'hf_home')
+        _hf_cache_dir = os.path.join(self.builddir, 'hf_cache')
+        _hf_cache_download_dir = os.path.join(_hf_cache_dir, 'downloads')
+
+        mkdir(_hf_home_dir)
+        mkdir(_hf_cache_dir)
         mkdir(_hf_cache_download_dir)
 
+        # Prepare download directory of cache_dir
         def _hash_url_to_filename(url):
             return run_shell_cmd(
                 f'python -c "import datasets; print(datasets.utils.file_utils.hash_url_to_filename(\'{url}\'))"'
-            ).output
+            ).output.strip()
 
         for src_spec in self.cfg['data_sources']:
             _url = f"hf://datasets/{self.cfg['hf_name']}@{self.cfg['hf_revision']}/{src_spec['filename']}"
@@ -77,38 +88,58 @@ class HuggingFaceDataset(Dataset):
             move_file(src_spec['filename'],  hash_filename)
             write_file(f"{hash_filename}.json", f'{{"url": "{_url}", "etag": null}}'.encode('utf-8'))
 
-        # Build actual dataset
-        _hf_home_dir = os.path.join(self.builddir, "hf_home")
-        load_arg_str = ", ".join([
-            f"{key}='{val}'"
-            for key, val in {
-                'path': self.cfg['hf_name'],
-                'revision': self.cfg['hf_revision'],
-                'cache_dir': self.builddir,
-                'download_mode': 'reuse_cache_if_exists',
-                'verification_mode': 'all_checks',
-            }.items()
-        ])
-        run_shell_cmd(f'HF_HOME={_hf_home_dir} python -c "from datasets import load_dataset; load_dataset({load_arg_str})"')
+        self.log.info(f"Successfully populated {_hf_cache_download_dir} from source files")
 
-        # Clean-up
-        clean_dir(_hf_home_dir)
-        clean_dir(_hf_cache_download_dir)
+        # Build actual dataset
+        py_script = '; '.join([
+            "import datasets",
+            f"""ds = datasets.load_dataset({
+                ', '.join([
+                    f"path='{self.cfg['hf_name']}'",
+                    f"revision='{self.cfg['hf_revision']}'",
+                    f"cache_dir='{_hf_cache_dir}'",
+                    "download_mode='reuse_cache_if_exists'",
+                    "verification_mode='all_checks'",
+                    "num_proc=1",
+                ])
+            })""",
+            f"ds.save_to_disk('{self._build_dataset_dir}')"
+        ])
+        result = run_shell_cmd(f'HF_HOME={_hf_home_dir} python -c "{py_script}"')
+
+        if any(
+            line.startswith(f"Downloading data:")
+            for line in result.output.splitlines()
+        ):
+            raise EasyBuildError('Unexpected download detected when loading dataset during build.')
+        else:
+            self.log.info(f"Successfully built dataset into {self._build_dataset_dir}")
+
+    def test_step(self):
+        '''Try loading dataset'''
+        with TemporaryDirectory(prefix=self.builddir) as _hf_home_dir:
+            py_script = '; '.join([
+                "from datasets import load_from_disk",
+                f"load_from_disk('{self._build_dataset_dir}')",
+            ])
+            run_shell_cmd(f'HF_HOME="{_hf_home_dir}" python -c "{py_script}"')
+
+            if dir_contains_files(_hf_home_dir):
+                report_test_failure(f'HF_HOME populated on load_dataset({dataset_dir}) call')
 
     def install_step(self):
         '''Move actual dataset directory to installdir'''
         change_dir(self.installdir)
-        dataset_dir = os.path.dirname(
-            find_glob_pattern(
-                os.path.join(
-                    self.builddir,
-                    self.cfg['hf_name'].replace('/', '___'),
-                    '*',  # default
-                    '*',  # version string
-                    '*',  # commit hash
-                    'dataset_info.json',
-                )
-            )
-        )
-        for dataset_file in expand_glob_paths([os.path.join(dataset_dir, '*')]):
-            move_file(dataset_file, os.path.join(self.installdir, os.path.basename(dataset_file)))
+        clean_dir(self.installdir)  # can't move dataset if dir already exists
+        move_file(self._build_dataset_dir, self.installdir)
+        if not dir_contains_files(self.installdir):
+            raise EasyBuildError('Install directory is empty, something has gone wrong.')
+        else:
+            self.log.info("Successfully moved dataset from builddir")
+        change_dir(self.installdir)
+
+    def post_processing_step(self):
+        # only for debugging
+        change_dir(self.installdir)
+        self.log.info("CWD = " + os.path.abspath(os.curdir))
+        super().post_processing_step()
