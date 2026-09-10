5b7df851acd9c513fdfba3ade9951287424afef0
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2026-09-10 11:21:06 +0200

    disable SVE on Grace Hopper

--- easybuild/easyblocks/l/lammps.py
+++ easybuild/easyblocks/l/lammps.py
@@ -435,6 +435,9 @@ class EB_LAMMPS(CMakeMake):
                     cuda_root = get_software_root('CUDA')
                     if get_cpu_architecture() == AARCH64 and LooseVersion(os.path.basename(cuda_root)) < '13.2.0':
                         self.cfg.update('configopts', '-D%s_ARCH_ARM_NEON=no' % self.kokkos_prefix)
+                        # For neoverse-v2 we need to also disable SVE
+                        if processor_arch == 'ARMV9_GRACE':
+                            self.cfg.update('configopts', '-D%s_ARCH_ARM_SVE=no' % self.kokkos_prefix)
                     self.cfg.update('configopts', '-D%s_ARCH_%s=yes' % (self.kokkos_prefix, gpu_arch))
                 else:
                     # Older versions of Kokkos required us to tweak the C++ compiler
