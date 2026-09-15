0f88c17005e589c83282e4249f3218d78570e645
sreenivasamurthy kolam <sreenivasamurthy.kolam@amd.com>
2026-09-15 16:20:26 +0530

    made changes to enable building openmp for 7.2.3 and maintain backward compatability with 6.4.1. In 6.4.1, when build_openmp and build_openmp_tools were set to true, it was adding rocm-device-libs as external projects in component 1 but this was having issues when building rocm-llvm-7.2.3.

--- easybuild/easyblocks/r/rocm_llvm.py
+++ easybuild/easyblocks/r/rocm_llvm.py
@@ -50,31 +50,38 @@ class EB_ROCm_minus_LLVM(EB_LLVM):
 
     def _configure_general_build(self):
         super(EB_ROCm_minus_LLVM, self)._configure_general_build()
-        self._cmakeopts.update({
-            'LLVM_EXTERNAL_PROJECTS': '"device-libs"',
-            'LLVM_EXTERNAL_DEVICE_LIBS_SOURCE_DIR': os.path.join(self.llvm_src_dir, 'amd', 'device-libs'),
+
+        cmake_opts = {
             'LLVM_ENABLE_PER_TARGET_RUNTIME_DIR': 'ON',
             'CLANG_DEFAULT_RTLIB': 'compiler-rt',
             'CLANG_DEFAULT_UNWINDLIB': 'libgcc',
             'DEFAULT_ROCM_PATH': self.installdir,
             'LIBOMP_COPY_EXPORTS': 'OFF',
             'CLANG_ENABLE_AMDCLANG': 'ON',
-            #'LIBOMPTARGET_EXTERNAL_PROJECT_ROCM_DEVICE_LIBS_PATH': self.installdir,
             'LIBOMPTARGET_EXTERNAL_PROJECT_ROCM_DEVICE_LIBS_PATH': os.path.join(self.llvm_src_dir, 'amd', 'device-libs'),
-        })
+        }
+
+        # Only set LLVM_EXTERNAL_PROJECTS if openmp/offload are NOT in runtimes
+        # When openmp/offload are present, the runtimes/CMakeLists.txt adds device-libs automatically
+        # For older versions without openmp/offload, we need to explicitly add device-libs
+        if 'openmp' not in self.final_runtimes and 'offload' not in self.final_runtimes:
+            cmake_opts['LLVM_EXTERNAL_PROJECTS'] = '"device-libs"'
+            cmake_opts['LLVM_EXTERNAL_DEVICE_LIBS_SOURCE_DIR'] = os.path.join(self.llvm_src_dir, 'amd', 'device-libs')
+
+        self._cmakeopts.update(cmake_opts)
 
         if LooseVersion('20') <= LooseVersion(self.version) <= LooseVersion('23'):
             # Set HSA path if ROCR-Runtime source directory exists
             rocr_runtime_path = os.path.join(self.start_dir, '..', 'ROCR-Runtime-rocm-7.2.3')
 
             if os.path.exists(rocr_runtime_path):
-                # Pass Clang_DIR to the ROCR external project so it can find ClangConfig.cmake
-                # Use the tools/clang path which is available during the build
-                clang_dir = os.path.join(self.llvm_obj_dir_stage1, 'tools', 'clang', 'cmake', 'modules')
+                # Pass CMAKE_PREFIX_PATH to the ROCR external project so it can find ClangConfig.cmake
+                # Use the build dir so cmake can find it after clang is built but before ROCR configures
+                cmake_prefix_path = os.path.join(self.llvm_obj_dir_stage1, 'lib', 'cmake')
 
                 self._cmakeopts.update({
                     'LIBOMPTARGET_EXTERNAL_PROJECT_HSA_PATH': rocr_runtime_path,
-                    'OFFLOAD_EXTERNAL_PROJECT_ROCR_CMAKE_ARGS': f'-DClang_DIR={clang_dir}'
+                    'OFFLOAD_EXTERNAL_PROJECT_ROCR_CMAKE_ARGS': f'-DCMAKE_PREFIX_PATH={cmake_prefix_path}'
                 })
             # Device-libs are always built in stage 1, regardless of bootstrap
             # So always use llvm_obj_dir_stage1 for device-libs path
