b1eada261e1144d75a604c945b8b1dee0dd63584
Bob Dröge <b.e.droge@rug.nl>
2026-09-21 19:24:10 +0200

    use 'not in' for membership test

--- easybuild/easyblocks/g/gromacs.py
+++ easybuild/easyblocks/g/gromacs.py
@@ -748,7 +748,7 @@ class EB_GROMACS(CMakeMake):
         bin_files = []
 
         dsuff = None
-        if not 'CUDA' in self.cfg.dependency_names():
+        if 'CUDA' not in self.cfg.dependency_names():
             for configopts in configopts_list:
                 # add the _d suffix to the suffix, in case of double precision
                 if self.double_prec_pattern in configopts:
